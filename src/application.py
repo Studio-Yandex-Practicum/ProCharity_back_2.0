fb9572a9c29fc1d920db48da308ddc099d0d21db
ojciec22 <ojciecwg@gmail.com>
2023-04-07 16:41:31 +0200

    fix root_path in application

--- src/application.py
+++ src/application.py
@@ -6,7 +6,7 @@ from src.settings import settings
 
 
 def create_app() -> FastAPI:
-    app = FastAPI(debug=settings.DEBUG, root_path=settings.ROOT_PATH)
+    app = FastAPI(debug=settings.DEBUG, root_path='')
     origins = ["*"]
 
     app.add_middleware(
