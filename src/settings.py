060d2bdb99fc339186a4bed0c39c255e9d43d1e0
albaranovsky <aleksey.baranovsky@gmail.com>
2023-04-09 11:39:08 +0300

    Add props: api_url and telegram_webhook_url

--- src/settings.py
+++ src/settings.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+from urllib.parse import urljoin
 
 from pydantic import BaseSettings
 from pydantic.tools import lru_cache
@@ -31,6 +32,15 @@ class Settings(BaseSettings):
             f"{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}"
             f"@{self.DB_HOST}:{self.DB_PORT}/{self.POSTGRES_DB}"
         )
+
+    @property
+    def api_url(self) -> str:
+        return urljoin(self.APPLICATION_URL, self.ROOT_PATH)
+
+    @property
+    def telegram_webhook_url(self) -> str:
+        """Получить url-ссылку на эндпоинт для работы telegram в режиме webhook."""
+        return urljoin(self.api_url, "telegram/webhook")
 
     class Config:
         env_file = ENV_FILE
