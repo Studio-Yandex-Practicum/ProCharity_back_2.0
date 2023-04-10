40348d0eeaf36de6f6a2a0c7a9f1495c7228457b
albaranovsky <aleksey.baranovsky@gmail.com>
2023-04-10 13:01:57 +0300

    Add event handlers for startup/shutdown

--- src/application.py
+++ src/application.py
@@ -2,11 +2,12 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from src.api import router
+from src.bot.bot import start_bot
 from src.settings import settings
 
 
 def create_app() -> FastAPI:
-    app = FastAPI(debug=settings.DEBUG, root_path='')
+    app = FastAPI(debug=settings.DEBUG, root_path="")
     origins = ["*"]
 
     app.add_middleware(
@@ -17,6 +18,25 @@ def create_app() -> FastAPI:
         allow_headers=["*"],
     )
 
-    app.include_router(router=router.router, prefix='/api')
+    app.include_router(router=router.router, prefix="/api")
+
+    @app.on_event("startup")
+    async def on_startup():
+        """Действия при запуске сервера."""
+        bot_instance = await start_bot()
+        # storing bot_instance to extra state of FastAPI app instance
+        # refer to https://www.starlette.io/applications/#storing-state-on-the-app-instance
+        app.state.bot_instance = bot_instance
+
+    @app.on_event("shutdown")
+    async def on_shutdown():
+        """Действия после остановки сервера."""
+        bot_instance = app.state.bot_instance
+        # manually stopping bot updater when running in polling mode
+        # see https://github.com/python-telegram-bot/python-telegram-bot/blob/master/telegram/ext/_application.py#L523
+        if not settings.BOT_WEBHOOK_MODE:
+            await bot_instance.updater.stop()
+        await bot_instance.stop()
+        await bot_instance.shutdown()
 
     return app
