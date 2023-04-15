caa68035c2f52b0eb3931ba1410e3a1673a7aec9
Alexey Krylov <na.derevnyu.elychu@gmail.com>
2023-04-15 16:40:08 +0300

    Отсылка сообщений. Исправления после ревью

--- src/bot/services.py
+++ src/bot/services.py
@@ -1,3 +1,4 @@
+import asyncio
 import logging
 
 from telegram.error import BadRequest, Forbidden, TelegramError
@@ -13,37 +14,26 @@ class TelegramNotification:
         self.__bot_application = telegram_bot
         self.__bot = telegram_bot.bot
 
+    async def __send_message(self, user_id: int, message: str) -> None:
+        try:
+            await self.__bot.send_message(chat_id=user_id, text=message)
+            logger.debug(f"Отправлено оповещение пользователю {user_id}")
+        except TelegramError as exc:
+            msg = f"Ошибка отправки сообщения пользователю {user_id}."
+            match exc:
+                case BadRequest():
+                    msg += " Некорректный id?"
+                case Forbidden():
+                    msg += " Бот заблокирован?"
+            msg += " " + exc.message
+            logger.info(msg)
+
     async def send_messages(
         self,
         message: str,
         users: list[User],
-        mark_user_for_banned_flag: bool = False,
     ) -> bool:
-        """Делает массовую рассылку сообщения message пользователям users.
-
-        Флаг mark_user_for_banned_flag указывает, обновлять ли в объекте модели
-        User поле banned, если отправка сообщения была неудачной по причине того,
-        что пользователь покинул чат с ботом (признак banned в модели).
-        """
-
-        def log_error(exception: TelegramError, user_id: int) -> str:
-            msg = f"Ошибка отправки сообщения пользователю {user_id}."
-            if type(exception) == BadRequest:
-                msg += " Некорректный id?"
-            if type(exception) == Forbidden:
-                msg += " Бот заблокирован?"
-            msg += " " + exception.message
-            return msg
-
-        count = 0
-        for user in users:
-            try:
-                await self.__bot.send_message(chat_id=user.telegram_id, text=message)
-                logger.debug(f"Отправлено оповещение пользователю {user.telegram_id}")
-                count += 1
-            except (BadRequest, Forbidden) as exc:
-                logger.info(log_error(exc, user.telegram_id))
-                if mark_user_for_banned_flag:
-                    user.banned = True
-        logger.debug(f"Оповещение отправлено в {str(count)} чатов.")
+        """Делает массовую рассылку сообщения message пользователям users."""
+        send_message_tasks = (self.__send_message(user.telegram_id, message) for user in users)
+        self.__bot_application.create_task(asyncio.gather(*send_message_tasks))
         return True
