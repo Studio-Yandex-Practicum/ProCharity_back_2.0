72ff72fc59c43625e4d593ca85f300e8d1b565ac
Sergei Banshchikov <s.o.banshchikov@yandex.ru>
2023-04-17 20:45:20 +0200

    Заканчивает связь моделей

--- test.py
+++ test.py
@@ -2,28 +2,36 @@ import asyncio
 import contextlib
 
 from src.core.db import get_session
-from src.core.db.models import Category, User
+from src.core.db.models import Category, User, Task
 
 
 async def main():
     get_async_session_context = contextlib.asynccontextmanager(get_session)
     async with get_async_session_context() as session:
+
         user = User(
-            # id=2,
-            telegram_id=1,
-            username="username1",
+            telegram_id=45,
+            username="username45",
+        )
+        task = Task(
+            title="Важный таск"
         )
         category = Category(
             name="test",
             archive=False,
             users=[user],
+            tasks=[task]
         )
+
         session.add(user)
+        session.add(task)
         session.add(category)
+
         await session.commit()
-        # await session.add_all((user, category))
+
         await session.delete(category)
         await session.delete(user)
+        await session.delete(task)
         await session.commit()
 
 if __name__ == "__main__":
