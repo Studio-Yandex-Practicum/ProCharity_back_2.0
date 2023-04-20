95f8677258c47b6343c4e4f49d26d72f4d03f76a
Sergei Banshchikov <s.o.banshchikov@yandex.ru>
2023-04-20 10:04:46 +0200

    Все модели переписаны на новом синтаксисе Алхимии2

--- src/core/db/models.py
+++ src/core/db/models.py
@@ -3,14 +3,15 @@ from sqlalchemy.ext.declarative import as_declarative
 from sqlalchemy.orm import relationship, Mapped, mapped_column, backref
 from sqlalchemy.sql import expression, func
 from sqlalchemy.sql.sqltypes import TIMESTAMP
+from datetime import date
 
 
 @as_declarative()
 class Base:
     """Базовая модель."""
 
-    id = Column(Integer, primary_key=True, unique=True)
-    __name__: str
+    id: Mapped[int] = mapped_column(primary_key=True)
+    __name__: Mapped[str]
 
 
 users_categories = Table(
@@ -25,16 +26,16 @@ class User(Base):
     """Модель пользователя."""
 
     __tablename__ = "users"
-    telegram_id = Column(BigInteger, unique=True)
-    username = Column(String(32), unique=True, nullable=True)
-    email = Column(String(48), unique=True, nullable=True)
-    external_id = Column(Integer, unique=True, nullable=True)
-    first_name = Column(String(64), nullable=True)
-    last_name = Column(String(64), nullable=True)
-    has_mailing = Column(Boolean, default=False)
-    date_registration = Column(TIMESTAMP, server_default=func.current_timestamp(), nullable=False)
-    external_signup_date = Column(TIMESTAMP, nullable=True)
-    banned = Column(Boolean, server_default=expression.false(), nullable=False)
+    telegram_id: Mapped[int] = mapped_column(BigInteger, unique=True)
+    username: Mapped[str] = mapped_column(String(32), unique=True, nullable=True)
+    email: Mapped[str] = mapped_column(String(48), unique=True, nullable=True)
+    external_id: Mapped[int] = mapped_column(unique=True, nullable=True)
+    first_name: Mapped[str] = mapped_column(String(64), nullable=True)
+    last_name: Mapped[str] = mapped_column(String(64), nullable=True)
+    has_mailing: Mapped[bool] = mapped_column(default=False)
+    date_registration: Mapped[date] = mapped_column(server_default=func.current_timestamp(), nullable=False)
+    external_signup_date: Mapped[date] = mapped_column(nullable=False)
+    banned: Mapped[bool] = mapped_column(server_default=expression.false(), nullable=False)
 
     categories: Mapped[list["Category"]] = relationship(
         secondary="users_categories", back_populates="users")
@@ -47,24 +48,25 @@ class Task(Base):
     """Модель задач."""
 
     __tablename__ = "tasks"
-    title = Column(String)
-    name_organization = Column(String)
-    deadline = Column(Date)
+    title = Mapped[str]
+    name_organization = Mapped[str]
+    deadline = Mapped[Date]
 
     category_id: Mapped[int] = mapped_column(ForeignKey("categories.id"))
     category: Mapped["Category"] = relationship(back_populates="tasks")
 
-    bonus = Column(Integer)
-    location = Column(String)
-    link = Column(String)
-    description = Column(String)
-    archive = Column(Boolean)
-    created_date = Column(TIMESTAMP, server_default=func.current_timestamp(), nullable=False)
-    updated_date = Column(
-        TIMESTAMP,
+    bonus = Mapped[int]
+    location = Mapped[str]
+    link = Mapped[str]
+    description = Mapped[str]
+    archive = Mapped[bool]
+    created_date: Mapped[date] = mapped_column(
+        server_default=func.current_timestamp(), nullable=False
+    )
+    updated_date: Mapped[date] = mapped_column(
         server_default=func.current_timestamp(),
         nullable=False,
-        onupdate=func.current_timestamp(),
+        onupdate=func.current_timestamp()
     )
 
     def __repr__(self):
@@ -75,8 +77,8 @@ class Category(Base):
     """Модель категорий."""
 
     __tablename__ = "categories"
-    name = Column(String(100))
-    archive = Column(Boolean())
+    name: Mapped[str] = mapped_column(String(100))
+    archive: Mapped[bool]
 
     users: Mapped[list["User"]] = relationship(
         secondary="users_categories", back_populates="categories"
@@ -84,14 +86,10 @@ class Category(Base):
 
     tasks: Mapped[list["Task"]] = relationship(back_populates="category")
 
-    parent_id = Column(Integer, ForeignKey("categories.id"), nullable=True)
+    parent_id: Mapped[int] = mapped_column(
+        Integer, ForeignKey("categories.id"), nullable=True)
     children = relationship(
-        "Category",
-        uselist=True,
-        backref=backref("parent", remote_side="Category.id"),
-        lazy="subquery",
-        join_depth=1,
-    )
+        "Category", backref=backref("parent", remote_side="Category.id"))
 
     def __repr__(self):
         return f"<Category {self.name}>"
