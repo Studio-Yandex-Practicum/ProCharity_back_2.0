4da3fa607766e73df9898aa51e0682d769ba300f
Vladimir <skalackii@yandex.ru>
2023-04-27 17:18:46 +0300

    a function to create menu buttons was created

--- src/bot/bot.py
+++ src/bot/bot.py
@@ -1,13 +1,15 @@
-from telegram.ext import AIORateLimiter, Application, CommandHandler
+from telegram.ext import AIORateLimiter, Application, CommandHandler, CallbackQueryHandler
 
 from src.settings import settings
 
-from .handlers import start_callback
+from .handlers import start_callback, menu_callback, button
 
 
 def create_bot() -> Application:
     bot = Application.builder().token(settings.BOT_TOKEN).rate_limiter(AIORateLimiter()).build()
     bot.add_handler(CommandHandler("start", start_callback))
+    bot.add_handler(CommandHandler("menu", menu_callback))
+    bot.add_handler(CallbackQueryHandler(button))
     return bot
 
 
--- src/bot/handlers.py
+++ src/bot/handlers.py
@@ -1,9 +1,44 @@
-from telegram import Update
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ContextTypes
 
+
+# ABOUT_PROECT = (f"C ProCharity профессионалы могут помочь некоммерческим"
+#     f" организациям в вопросах, которые требуют специальных знаний и опыта."
+#     f"\n Интеллектуальный волонтер безвозмездно дарит фонду свое время и"
+#     f" профессиональные навыки, позволяя решиать задачи, которые трудно"
+#     f" закрыть  силами штатных сотрудников."
+# )
 
 async def start_callback(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(
         chat_id=update.effective_chat.id,
         text="Привет! Это бот платформы интеллектуального волонтерства " "ProCharity",
     )
+
+
+async def menu_callback(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    """Create button menu."""
+    keyboard = [
+        [InlineKeyboardButton("Посмотреть открытые задания", callback_data="1")],
+        [InlineKeyboardButton("Изменить компетенции", callback_data="2")],
+        [InlineKeyboardButton("Отправить предложение/ошибку", callback_data="3")],
+        [InlineKeyboardButton("Задать свой вопрос", callback_data="4")],
+        [InlineKeyboardButton("О платформе", callback_data="ABOUT_PROECT")],
+        [InlineKeyboardButton(
+            " ⏹️ Остановить / ▶️ включить подписку на задания", callback_data="3"
+        )],
+    ]
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    await update.message.reply_text(
+        "Please choose:", reply_markup=reply_markup
+    )
+
+
+async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Parses the CallbackQuery and updates the message text."""
+    query = update.callback_query
+    await query.answer()
+
+    await query.edit_message_text(text=f"Selected option: {query.data}")
