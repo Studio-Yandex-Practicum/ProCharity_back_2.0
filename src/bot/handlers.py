07c2c40cd515eb21b9a9c1847050b1146ea97062
Vladimir <skalackii@yandex.ru>
2023-04-29 14:01:35 +0300

    fix callback

--- src/bot/handlers.py
+++ src/bot/handlers.py
@@ -2,13 +2,6 @@ from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ContextTypes
 
 
-# ABOUT_PROECT = (f"C ProCharity профессионалы могут помочь некоммерческим"
-#     f" организациям в вопросах, которые требуют специальных знаний и опыта."
-#     f"\n Интеллектуальный волонтер безвозмездно дарит фонду свое время и"
-#     f" профессиональные навыки, позволяя решиать задачи, которые трудно"
-#     f" закрыть  силами штатных сотрудников."
-# )
-
 async def start_callback(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(
         chat_id=update.effective_chat.id,
@@ -19,20 +12,36 @@ async def start_callback(update: Update, context: ContextTypes.DEFAULT_TYPE):
 async def menu_callback(update: Update, context: ContextTypes.DEFAULT_TYPE):
     """Create button menu."""
     keyboard = [
-        [InlineKeyboardButton("Посмотреть открытые задания", callback_data="1")],
-        [InlineKeyboardButton("Изменить компетенции", callback_data="2")],
-        [InlineKeyboardButton("Отправить предложение/ошибку", callback_data="3")],
-        [InlineKeyboardButton("Задать свой вопрос", callback_data="4")],
-        [InlineKeyboardButton("О платформе", callback_data="ABOUT_PROECT")],
         [InlineKeyboardButton(
-            " ⏹️ Остановить / ▶️ включить подписку на задания", callback_data="3"
+            "Посмотреть открытые задания",
+            callback_data="view_tasks"
+        )],
+        [InlineKeyboardButton(
+            "Изменить компетенции",
+            callback_data="change_category"
+        )],
+        [InlineKeyboardButton(
+            "Отправить предложение/ошибку",
+            callback_data="send_error_or_proposal"
+        )],
+        [InlineKeyboardButton(
+            "Задать свой вопрос",
+            callback_data="ask_your_question"
+        )],
+        [InlineKeyboardButton(
+            "О платформе",
+            callback_data="about_project"
+        )],
+        [InlineKeyboardButton(
+            "⏹️ Остановить / ▶️ включить подписку на задания",
+            callback_data="job_subscription"
         )],
     ]
 
     reply_markup = InlineKeyboardMarkup(keyboard)
 
     await update.message.reply_text(
-        "Please choose:", reply_markup=reply_markup
+        "Выбери, что тебя интересует:", reply_markup=reply_markup
     )
 
 
