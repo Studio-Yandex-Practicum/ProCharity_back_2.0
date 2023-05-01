ec50baa65b9b16a2512d53667d6071effcd792ac
Андрей Янковский <tiamoon@yandex.ru>
2023-05-01 18:45:49 +0300

    добавление аргументов к функции

--- src/core/utils.py
+++ src/core/utils.py
@@ -3,8 +3,8 @@ from functools import wraps
 
 async def auto_commit(func):
     @wraps(func)
-    async def auto_commit_wraps(self, commit=True):
+    async def auto_commit_wraps(self, commit=True, *args, **kwargs):
         if commit:
             await self._session.commit()
-        return func
+        return await func(*args, **kwargs)
     return auto_commit_wraps
