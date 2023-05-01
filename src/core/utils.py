332e643ec2042ed572d871d677bda5a9c9976a4c
Андрей Янковский <tiamoon@yandex.ru>
2023-05-01 22:17:06 +0300

    протестированно

--- src/core/utils.py
+++ src/core/utils.py
@@ -1,10 +1,10 @@
 from functools import wraps
 
 
-async def auto_commit(func):
+def auto_commit(func):
     @wraps(func)
-    async def auto_commit_wraps(self, commit=True, *args, **kwargs):
+    async def auto_commit_wraps(self, commit=True):
         if commit:
             await self._session.commit()
-        return await func(*args, **kwargs)
+        return func
     return auto_commit_wraps
