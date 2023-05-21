78d21070c1190508edc8eda6c6e9f5004efad8d5
Maria Shipitsyna <m.fix003@gmail.com>
2023-05-21 20:34:22 +0300

    Add transaction for ContentService (Update)

--- src/api/services/base.py
+++ src/api/services/base.py
@@ -1,21 +1,24 @@
+from contextlib import asynccontextmanager
+
 from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
-
-from src.api.schemas import CategoryRequest, TaskRequest
-from src.core.db.db import get_session
 
 
 class ContentService:
     """Базовый класс для контента."""
 
-    def __init__(self, repository: any = Depends(), session: AsyncSession = Depends(get_session)):
+    def __init__(self, repository: any = Depends()):
         self._repository = repository
-        self._session = session
 
-    async def actualize_objects(self, objects: list[CategoryRequest | TaskRequest], model_class: any) -> None:
-        to_create, to_update = [], []
-        with self._session.begin() as session:
-            await self._repository.archive_all(commit=False)
+    @asynccontextmanager
+    async def get_async_session(self) -> AsyncSession:
+        async with AsyncSession() as session:
+            yield session
+
+    async def actualize_objects(self, objects: list[any], model_class: any) -> None:
+        async with self.get_async_session():
+            to_create, to_update = [], []
+            await self._repository.archive_all()
             already_have = await self._repository.get_all_ids()
             for obj in objects:
                 if obj.id not in already_have:
@@ -23,10 +26,9 @@ class ContentService:
                 else:
                     to_update.append({**obj.dict(), "archive": False})
             if to_create:
-                await self._repository.create_all(to_create, commit=False)
+                await self._repository.create_all(to_create)
             if to_update:
-                await self._repository.update_all(to_update, commit=False)
-            session.commit()
+                await self._repository.update_all(to_update)
 
     async def get_all(self) -> list[any]:
         return await self._repository.get_all()
