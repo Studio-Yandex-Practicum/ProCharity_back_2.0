cbdab58c7481475570385ce48f828352a7a8cf29
Anya S <shipkov.alexey@gmail.com>
2023-05-26 18:27:42 +0300

    add root_path api

--- src/application.py
+++ src/application.py
@@ -12,7 +12,7 @@ from src.settings import settings
 def create_app() -> FastAPI:
     app = FastAPI(
         debug=settings.DEBUG,
-        root_path=settings.ROOT_PATH)
+        root_path="/api/")
     origins = ["*"]
 
     app.add_middleware(
