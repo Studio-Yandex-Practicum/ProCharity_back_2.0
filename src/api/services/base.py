7cb0d956b047ac9e1de790237d21a3f54fc6858d
Maria Shipitsyna <m.fix003@gmail.com>
2023-05-26 21:45:13 +0500

    Syntax fixed

--- src/api/services/base.py
+++ src/api/services/base.py
@@ -16,7 +16,7 @@ class ContentService(abc.ABC):
     async def actualize_objects(self, objects: list[any], model_class: any) -> None:
         to_create, to_update = [], []
         ids = [obj.id for obj in objects]
-        async with self._session as session:
+        async with self._session.begin() as session:
             await self._repository.archive_by_ids(ids, commit=False)
             already_have = await self._repository.get_all_non_archived_and_by_ids(ids)
             for obj in objects:
@@ -32,4 +32,3 @@ class ContentService(abc.ABC):
 
     async def get_all(self) -> list[any]:
         return await self._repository.get_all()
-    
