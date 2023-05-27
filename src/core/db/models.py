5c03e3b77201dda1a81dea32c7d9665de0af980f
Levon Gegamyan <levongeg@gmail.com>
2023-05-27 16:12:04 +0300

    add UsersCategories model

--- src/core/db/models.py
+++ src/core/db/models.py
@@ -1,12 +1,18 @@
 from datetime import date
 
-from sqlalchemy import BigInteger, Column, Date, ForeignKey, Integer, String, Table, UniqueConstraint
+from sqlalchemy import BigInteger, Date, ForeignKey, Integer, String
 from sqlalchemy.orm import DeclarativeBase, Mapped, backref, mapped_column, relationship
 from sqlalchemy.sql import expression, func
 
 
 class Base(DeclarativeBase):
     """Основа для базового класса."""
+    __name__: Mapped[str]
+
+
+class EntityBase(Base):
+    """Базовый класс для сущностей."""
+    __abstract__ = True
 
     id: Mapped[int] = mapped_column(primary_key=True)
     created_at: Mapped[date] = mapped_column(server_default=func.current_timestamp(), nullable=False)
@@ -15,19 +21,21 @@ class Base(DeclarativeBase):
         nullable=False,
         onupdate=func.current_timestamp(),
     )
-    __name__: Mapped[str]
 
 
-users_categories = Table(
-    "users_categories",
-    Base.metadata,
-    Column("category_id", ForeignKey("categories.id"), primary_key=True),
-    Column("user_id", ForeignKey("users.id"), primary_key=True),
-    UniqueConstraint('category_id', 'user_id', name='unique_user_category')
-)
+class UsersCategories(Base):
+    """Модель отношений пользователь-категория."""
+
+    __tablename__ = "users_categories"
+
+    category_id: Mapped[int] = mapped_column(Integer, ForeignKey("categories.id"), primary_key=True)
+    user_id: Mapped[int] = mapped_column(Integer, ForeignKey("users.id"), primary_key=True)
+
+    def __repr__(self):
+        return f"<User {self.user_id} - Category {self.category_id}>"
 
 
-class User(Base):
+class User(EntityBase):
     """Модель пользователя."""
 
     __tablename__ = "users"
@@ -41,13 +49,15 @@ class User(Base):
     external_signup_date: Mapped[date] = mapped_column(nullable=True)
     banned: Mapped[bool] = mapped_column(server_default=expression.false(), nullable=False)
 
-    categories: Mapped[list["Category"]] = relationship(secondary="users_categories", back_populates="users")
+    categories: Mapped[list["Category"]] = relationship(
+        "Category", secondary="users_categories", back_populates="users"
+    )
 
     def __repr__(self):
         return f"<User {self.telegram_id}>"
 
 
-class Task(Base):
+class Task(EntityBase):
     """Модель задач."""
 
     __tablename__ = "tasks"
@@ -68,14 +78,16 @@ class Task(Base):
         return f"<Task {self.title}>"
 
 
-class Category(Base):
+class Category(EntityBase):
     """Модель категорий."""
 
     __tablename__ = "categories"
     name: Mapped[str] = mapped_column(String(100))
     is_archived: Mapped[bool]
 
-    users: Mapped[list["User"]] = relationship(secondary="users_categories", back_populates="categories")
+    users: Mapped[list["User"]] = relationship(
+        "User", secondary="users_categories", back_populates="categories"
+    )
 
     tasks: Mapped[list["Task"]] = relationship(back_populates="category")
 
