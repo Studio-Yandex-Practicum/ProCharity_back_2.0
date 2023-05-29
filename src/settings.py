9a3dd66bef9662fbbb1aacea9a4d31a0e388be09
= <=>
2023-05-29 22:59:22 +0300

    Revert "Fixed the dotenv import error"
    This reverts commit f6ba92dce6b145f0c775f512b562114436a7e6fb.

--- src/settings.py
+++ src/settings.py
@@ -5,26 +5,16 @@ from pydantic import BaseSettings, validator
 from pydantic.tools import lru_cache
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-@lru_cache()
-def get_env_file() -> Path | None:
-    import importlib
-
-    try:
-        importlib.import_module("dotenv")
-    except ImportError:
-        return
-
-    if Path.exists(BASE_DIR / ".env"):
-        return BASE_DIR / ".env"
+ENV_FILE = None
+if Path.exists(BASE_DIR / ".env"):
+    ENV_FILE = BASE_DIR / ".env"
 
 
 class Settings(BaseSettings):
     """Настройки проекта."""
 
     APPLICATION_URL: str = "localhost"
-    SECRET_KEY: str
+    SECRET_KEY: str 
     ROOT_PATH: str = "/api/"
     DEBUG: bool = False
 
@@ -83,7 +73,7 @@ class Settings(BaseSettings):
         return BASE_DIR / "src" / "bot" / "templates" / "feedback_form.html"
 
     class Config:
-        env_file = get_env_file()
+        env_file = ENV_FILE
 
 
 @lru_cache()
