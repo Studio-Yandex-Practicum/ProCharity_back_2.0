e3c3040fe4a1785cd09e15953022a87f3c12a0c4
= <=>
2023-05-29 23:59:36 +0300

    refactoring flake8 issues

--- src/application.py
+++ src/application.py
@@ -10,9 +10,7 @@ from src.settings import settings
 
 
 def create_app() -> FastAPI:
-    app = FastAPI(
-        debug=settings.DEBUG,
-        root_path="/api")
+    app = FastAPI(debug=settings.DEBUG, root_path="/api")
     origins = ["*"]
 
     app.add_middleware(
