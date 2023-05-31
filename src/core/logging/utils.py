27de24e9e76b54198a217f278fc95810c16da33b
Konstantin Korneev <konstantinkorneev@MacBook-Air-Konstantin.local>
2023-05-31 22:46:34 +0300

    feature/bot_update_logging

--- src/core/logging/utils.py
+++ src/core/logging/utils.py
@@ -1,11 +1,18 @@
 import structlog
 
+
+async def not_handled_updates_func(func, *args):
+    if args is None:
+        await log.ainfo(f"Не пойманны updates для функции {func.__name__}")
+
+
 log = structlog.get_logger()
 
 
 def logger_decor(func):
-    def wrapper(update, context):
-        log.info(f"Получено обновление: Update - {update}; Context - {context}")
-        return func(update, context)
-
+    async def wrapper(*args, **kwargs):
+        await log.ainfo(f"Запущенна функция {func.__name__}", args=args, kwargs=kwargs)
+        await log.ainfo(f"Получено обновление: args = {args}, kwargs = {kwargs}")
+        await not_handled_updates_func(func, *args)
+        return await func(*args, **kwargs)
     return wrapper
