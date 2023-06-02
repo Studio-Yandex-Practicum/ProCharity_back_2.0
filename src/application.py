09a539df18728897cc2488fa2e6519685b566634
= <=>
2023-06-02 07:26:44 +0300

    Revert "fixed urls"
    This reverts commit f6141df82aaf10674cb2a12dfbe4bd0388966f04.

--- src/application.py
+++ src/application.py
@@ -12,6 +12,7 @@ from src.settings import settings
 def create_app() -> FastAPI:
     app = FastAPI(
         debug=settings.DEBUG,
+        root_path=settings.ROOT_PATH,
     )
     origins = ["*"]
 
