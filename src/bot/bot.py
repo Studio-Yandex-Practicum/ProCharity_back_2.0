9308cec5d72cedacd91d2696850f3e954f919efc
Konstantin Korneev <konstantinkorneev@MacBook-Air-Konstantin.local>
2023-06-05 17:21:44 +0300

    Logging uncaught updates

--- src/bot/bot.py
+++ src/bot/bot.py
@@ -24,6 +24,7 @@ def create_bot() -> Application:
     bot.add_handler(CallbackQueryHandler(handlers.back_subcategory_callback, pattern=patterns.BACK_SUBCATEGORY))
     bot.add_handler(TypeHandler(Update, logging_updates))
 
+
     return bot
 
 
