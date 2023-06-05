83452337941d68016c2cc77adf04aa6ce85a13cb
Konstantin Korneev <konstantinkorneev@MacBook-Air-Konstantin.local>
2023-06-05 17:11:48 +0300

    Logging uncaught updates

--- src/bot/bot.py
+++ src/bot/bot.py
@@ -4,35 +4,24 @@ from telegram import Update
 from telegram.ext import AIORateLimiter, Application, CallbackQueryHandler, CommandHandler, MessageHandler, TypeHandler
 from telegram.ext.filters import StatusUpdate
 
-from src.bot.constants.callback_data import ASK_YOUR_QUESTION, CONFIRM_CATEGORIES, SEND_ERROR_OR_PROPOSAL
-from src.bot.handlers import (
-    ask_your_question,
-    back_subcategory_callback,
-    categories_callback,
-    confirm_categories_callback,
-    menu_callback,
-    select_subcategory_callback,
-    start_command,
-    subcategories_callback,
-    web_app_data,
-)
+from src.bot import handlers
+from src.bot.constants import callback_data, commands, patterns
 from src.core.logging.utils import logging_updates
 from src.settings import settings
 
 
 def create_bot() -> Application:
     bot = Application.builder().token(settings.BOT_TOKEN).rate_limiter(AIORateLimiter()).build()
-    bot.add_handler(CommandHandler("start", start_command))
-    bot.add_handler(CommandHandler("menu", menu_callback))
-    bot.add_handler(CommandHandler("categories", categories_callback))
-    bot.add_handler(CallbackQueryHandler(categories_callback, pattern="change_category"))
-    bot.add_handler(CallbackQueryHandler(subcategories_callback, pattern=r"category_(\d+)"))
-    bot.add_handler(CallbackQueryHandler(ask_your_question, pattern=ASK_YOUR_QUESTION))
-    bot.add_handler(CallbackQueryHandler(ask_your_question, pattern=SEND_ERROR_OR_PROPOSAL))
-    bot.add_handler(MessageHandler(StatusUpdate.WEB_APP_DATA, web_app_data))
-    bot.add_handler(CallbackQueryHandler(select_subcategory_callback, pattern=r"select_category_(\d+)"))
-    bot.add_handler(CallbackQueryHandler(back_subcategory_callback, pattern=r"back_to_(\d+)"))
-    bot.add_handler(CallbackQueryHandler(confirm_categories_callback, pattern=CONFIRM_CATEGORIES))
+    bot.add_handler(CommandHandler(commands.START, handlers.start_command))
+    bot.add_handler(CommandHandler(commands.MENU, handlers.menu_callback))
+    bot.add_handler(CallbackQueryHandler(handlers.categories_callback, pattern=callback_data.CHANGE_CATEGORY))
+    bot.add_handler(CallbackQueryHandler(handlers.categories_callback, pattern=callback_data.GET_CATEGORIES))
+    bot.add_handler(CallbackQueryHandler(handlers.subcategories_callback, pattern=patterns.SUBCATEGORIES))
+    bot.add_handler(CallbackQueryHandler(handlers.ask_your_question, pattern=callback_data.ASK_YOUR_QUESTION))
+    bot.add_handler(CallbackQueryHandler(handlers.ask_your_question, pattern=callback_data.SEND_ERROR_OR_PROPOSAL))
+    bot.add_handler(MessageHandler(StatusUpdate.WEB_APP_DATA, handlers.web_app_data))
+    bot.add_handler(CallbackQueryHandler(handlers.select_subcategory_callback, pattern=patterns.SELECT_CATEGORY))
+    bot.add_handler(CallbackQueryHandler(handlers.back_subcategory_callback, pattern=patterns.BACK_SUBCATEGORY))
     bot.add_handler(TypeHandler(Update, logging_updates))
 
     return bot
---
+++ src/bot/constants/patterns.py
@@ -0,0 +1,3 @@
+SUBCATEGORIES = r"category_\d+"
+SELECT_CATEGORY = r"select_category_(\d+)"
+BACK_SUBCATEGORY = r"back_to_(\d+)"
