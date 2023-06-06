da61d5924d6e74072edbeea5ba1fb69071789b2a
= <=>
2023-06-06 23:59:56 +0300

    fixed deploy

--- src/core/utils.py
+++ src/core/utils.py
@@ -1,9 +1,6 @@
 import sys
 from functools import wraps
 
-from pyngrok import ngrok
-
-from src.core.logging.setup import setup_logging
 from src.settings import settings
 
 
@@ -19,6 +16,7 @@ def auto_commit(func):
 
 
 def set_ngrok():
+    from pyngrok import ngrok
+
     port = sys.argv[sys.argv.index("--port") + 1] if "--port" in sys.argv else 8000
     settings.APPLICATION_URL = ngrok.connect(port).public_url
-    setup_logging()
