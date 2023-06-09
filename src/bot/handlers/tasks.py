f196952f8217197027d4fb5f05d57850efc6da7c
Levon Gegamyan <levongeg@gmail.com>
2023-06-09 12:42:40 +0300

    fix after merge

--- src/bot/handlers/tasks.py
+++ src/bot/handlers/tasks.py
@@ -5,29 +5,34 @@ from telegram.ext import Application, CallbackContext, CallbackQueryHandler, Con
 from src.bot.constants import callback_data, patterns
 from src.bot.keyboards import get_categories_keyboard, get_subcategories_keyboard
 from src.bot.services.task import TaskService
+from src.bot.services.category import CategoryService
 from src.core.logging.utils import logger_decor
 from src.core.utils import display_tasks
 
 
 @logger_decor
 async def subcategories_callback(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    category_service = CategoryService()
     query = update.callback_query
     parent_id = int(context.match.group(1))
     context.user_data["parent_id"] = parent_id
+    subcategories = await category_service.get_unarchived_subcategories(parent_id)
+    selected_categories = context.user_data.get("selected_categories", {})
 
     await query.message.edit_text(
         "Чтобы я знал, с какими задачами ты готов помогать, "
         "выбери свои профессиональные компетенции (можно выбрать "
         'несколько). После этого, нажми на пункт "Готово 👌"',
-        reply_markup=await get_subcategories_keyboard(parent_id, context),
+        reply_markup=await get_subcategories_keyboard(parent_id, subcategories, selected_categories),
     )
 
 
 @logger_decor
 async def select_subcategory_callback(update: Update, context: ContextTypes.DEFAULT_TYPE):
     query = update.callback_query
+    category_service = CategoryService()
     subcategory_id = int(context.match.group(1))
-    selected_categories = context.user_data.setdefault("selected_categories", {})
+    selected_categories = context.user_data.get("selected_categories", {})
 
     if subcategory_id not in selected_categories:
         selected_categories[subcategory_id] = None
@@ -35,24 +40,27 @@ async def select_subcategory_callback(update: Update, context: ContextTypes.DEFA
         del selected_categories[subcategory_id]
 
     parent_id = context.user_data["parent_id"]
+    subcategories = await category_service.get_unarchived_subcategories(parent_id)
 
     await query.message.edit_text(
         "Чтобы я знал, с какими задачами ты готов помогать, "
         "выбери свои профессиональные компетенции (можно выбрать "
         'несколько). После этого, нажми на пункт "Готово 👌"',
-        reply_markup=await get_subcategories_keyboard(parent_id, context),
+        reply_markup=await get_subcategories_keyboard(parent_id, subcategories, selected_categories),
     )
 
 
 @logger_decor
 async def back_subcategory_callback(update: Update, context: ContextTypes.DEFAULT_TYPE):
     query = update.callback_query
+    category_service = CategoryService()
+    categories = await category_service.get_unarchived_parents()
 
     await query.message.edit_text(
         "Чтобы я знал, с какими задачами ты готов помогать, "
         "выбери свои профессиональные компетенции (можно выбрать "
         'несколько). После этого, нажми на пункт "Готово 👌"',
-        reply_markup=await get_categories_keyboard(),
+        reply_markup=await get_categories_keyboard(categories),
     )
 
 
