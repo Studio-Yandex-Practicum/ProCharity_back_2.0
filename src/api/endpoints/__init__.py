b512021aaa789db556209a8aeb90215718e22b4e
vadim <vadimbor93@mail.ru>
2023-06-26 17:59:36 +0300

    добавлен роутер users_group_notification_router

--- src/api/endpoints/__init__.py
+++ src/api/endpoints/__init__.py
@@ -1,5 +1,6 @@
 from .categories import category_router
 from .form import form_router
 from .tasks import task_router
+from .notification import users_group_notification_router
 
-__all__ = ("category_router", "task_router", "form_router")
+__all__ = ("category_router", "task_router", "form_router", "users_group_notification_router",)
