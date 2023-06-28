a5bf57c78245ae8ad83020a9174a3f5c1258a2dd
vadim <vadimbor93@mail.ru>
2023-06-28 19:37:38 +0300

    Передал на конструкцию match/case с применением класса StrEnum

--- src/api/services/messages.py
+++ src/api/services/messages.py
@@ -2,14 +2,14 @@ from fastapi import Depends
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.api.services.base import ContentService
 from src.bot.bot import create_bot
 from src.core.db import get_session
 from src.core.db.models import User
+from src.core.enums import TelegramNotificationUsersGroups
 from src.core.services.notification import TelegramNotification
 
 
-class TelegramNotificationService(ContentService):
+class TelegramNotificationService:
     """Класс описывающий функционал передачи сообщения
       определенному пользователю """
 
@@ -21,13 +21,13 @@ class TelegramNotificationService(ContentService):
 
     async def send_messages_to_group_of_users(self, notifications):
         """ Отправляет сообщение указанной группе пользователей"""
-        if notifications.mode == "all":
-            users = await self._session.execute(select(User))
-        if notifications.mode == "subscribed":
-            users = await self._session.execute(select(User).where(User.has_mailing == True)) # noqa
-        if notifications.mode == "unsubscribed":
-            users = await self._session.execute(select(User).where(User.has_mailing == False)) # noqa
-
+        match notifications.mode.upper():
+            case TelegramNotificationUsersGroups.ALL.name:
+                users = await self._session.execute(select(User))
+            case TelegramNotificationUsersGroups.SUBSCRIBED.name:
+                users = await self._session.execute(select(User).where(User.has_mailing == True)) # noqa
+            case TelegramNotificationUsersGroups.UNSUBSCRIBED.name:
+                users = await self._session.execute(select(User).where(User.has_mailing == False)) # noqa
         users = users.scalars().all()
         telegram_notification = TelegramNotification(create_bot())
         await telegram_notification.send_messages(message=notifications.message, users=users)
