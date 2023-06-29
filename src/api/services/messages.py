04f9a8d53ecd2062a19bc4b78ba8622de28b5a26
vadim <vadimbor93@mail.ru>
2023-06-29 11:40:39 +0300

    Вынес create_bot в DI / переделал запросы в базу на session.scalars(<query>)

--- src/api/services/messages.py
+++ src/api/services/messages.py
@@ -1,6 +1,7 @@
 from fastapi import Depends
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
+from telegram.ext import Application
 
 from src.bot.bot import create_bot
 from src.core.db import get_session
@@ -15,19 +16,20 @@ class TelegramNotificationService:
 
     def __init__(
         self,
+        telegram_bot: Application = Depends(create_bot),
         session: AsyncSession = Depends(get_session),
     ) -> None:
         self._session = session
+        self.telegram_bot = telegram_bot
 
     async def send_messages_to_group_of_users(self, notifications):
         """Отправляет сообщение указанной группе пользователей"""
         match notifications.mode.upper():
             case TelegramNotificationUsersGroups.ALL.name:
-                users = await self._session.execute(select(User))
+                users = await self._session.scalars(select(User))
             case TelegramNotificationUsersGroups.SUBSCRIBED.name:
-                users = await self._session.execute(select(User).where(User.has_mailing == True))  # noqa
+                users = await self._session.scalars(select(User).where(User.has_mailing == True))  # noqa
             case TelegramNotificationUsersGroups.UNSUBSCRIBED.name:
-                users = await self._session.execute(select(User).where(User.has_mailing == False))  # noqa
-        users = users.scalars().all()
-        telegram_notification = TelegramNotification(create_bot())
+                users = await self._session.scalars(select(User).where(User.has_mailing == False))  # noqa
+        telegram_notification = TelegramNotification(self.telegram_bot)
         await telegram_notification.send_messages(message=notifications.message, users=users)
