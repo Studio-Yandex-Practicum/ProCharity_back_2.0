6509400661ce6309250c21db6ba375a58915513a
vadim <vadimbor93@mail.ru>
2023-07-19 16:02:26 +0300

    Добавлена фильтрация задач по пользователю и не архивным задачам

--- src/core/db/repository/task.py
+++ src/core/db/repository/task.py
@@ -1,10 +1,10 @@
 from fastapi import Depends
-from sqlalchemy import select
+from sqlalchemy import select, and_
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import joinedload
 
 from src.core.db.db import get_session
-from src.core.db.models import Category, Task
+from src.core.db.models import Category, Task, User
 from src.core.db.repository.base import ContentRepository
 
 
@@ -25,11 +25,23 @@ class TaskRepository(ContentRepository):
         """Получить список задач из категорий на которые подписан пользователь."""
         return await self._session.scalars(select(Task).options(joinedload(Task.category)))
 
-    async def get_tasks_limit_for_user(self, limit: int, offset: int) -> list[Task]:
+    async def get_tasks_limit_for_user(self, limit: int, offset: int, user_telegram_id: int) -> list[Task]:
         """Получить limit-выборку из списка всех задач пользователя."""
-        return await self._session.scalars(select(Task).options(joinedload(Task.category)).limit(limit).offset(offset))
+        user_id = await self._session.scalar((select(User.id).where(User.telegram_id == user_telegram_id)))
+        return await self._session.scalars(
+            select(Task)
+            .join(Category)
+            .options(joinedload(Task.category))
+            .where(and_(Category.users.any(id=user_id)), Task.is_archived == False) # noqa
+            .limit(limit)
+            .offset(offset))
 
-    async def get_user_tasks_count(self) -> int:
+    async def get_user_tasks_count(self, user_telegram_id: int) -> int:
         """Получить общее количество задач для пользователя."""
-        tasks = await self._session.scalars(select(Task).options(joinedload(Task.category)))
+        user_id = await self._session.scalar((select(User.id).where(User.telegram_id == user_telegram_id)))
+        tasks = await self._session.scalars(
+            select(Task)
+            .join(Category)
+            .options(joinedload(Task.category))
+            .where(and_(Category.users.any(id=user_id)), Task.is_archived == False)) # noqa
         return len(tasks.all())
