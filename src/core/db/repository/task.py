e287eb120dcdb2feb799bdb226a42300c321fb07
vadim <vadimbor93@mail.ru>
2023-07-19 11:41:45 +0300

    Переработана функция get_tasks_for_user_in_telegram - возвращает в телеграме таски по категориям на которые подписан пользователь

--- src/core/db/repository/task.py
+++ src/core/db/repository/task.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import joinedload
 
 from src.core.db.db import get_session
-from src.core.db.models import Category, Task
+from src.core.db.models import Category, Task, User
 from src.core.db.repository.base import ContentRepository
 
 
@@ -19,6 +19,12 @@ class TaskRepository(ContentRepository):
         tasks = await self._session.execute(select(Task).join(Category).where(Category.users.any(id=user_id)))
         return tasks.scalars().all()
 
-    async def get_user_tasks(self, limit: int) -> list[Task]:
-        """Получить список задач из категорий."""
-        return await self._session.scalars(select(Task).options(joinedload(Task.category)).limit(limit))
+    async def get_tasks_for_user_in_telegram(self, user_telegram_id: int, limit: int) -> list[Task]:
+        """Получить список задач в телеграм боте из категорий на которые подписан пользователь."""
+        user_id = await self._session.scalar((select(User.id).where(User.telegram_id == user_telegram_id)))
+        return await self._session.scalars(
+            select(Task)
+            .join(Category)
+            .options(joinedload(Task.category))
+            .where(Category.users.any(id=user_id))
+            .limit(limit))
