55f97d0c958ef8faab8345db156dd2fd4ffabc26
vadim <vadimbor93@mail.ru>
2023-07-21 16:47:03 +0300

    get_tasks_for_user - добавлены дефолтные значения limit и offset для фильтрации по api |     async def get_tasks_limit_for_user(self, limit: int, offset: int, user: User) -> list[Task]:

--- src/core/db/repository/task.py
+++ src/core/db/repository/task.py
@@ -1,5 +1,5 @@
 from fastapi import Depends
-from sqlalchemy import and_, select
+from sqlalchemy import select, func
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import joinedload
 from sqlalchemy.sql.expression import false
@@ -15,10 +15,15 @@ class TaskRepository(ContentRepository):
     def __init__(self, session: AsyncSession = Depends(get_session)) -> None:
         super().__init__(session, Task)
 
-    async def get_tasks_for_user(self, user_id: int, limit: int, offset: int) -> list[Task]:
+    async def get_tasks_for_user(self, user_id: int, limit: int = 3, offset: int = 0) -> list[Task]:
         """Получить список задач из категорий на которые подписан пользователь."""
         tasks = await self._session.execute(
-            select(Task).join(Category).where(Category.users.any(id=user_id)).limit(limit).offset(offset)
+            select(Task)
+            .join(Category)
+            .where(Category.users.any(id=user_id))
+            .where(Task.is_archived == false())
+            .limit(limit)
+            .offset(offset)
         )
         return tasks.scalars().all()
 
@@ -26,25 +31,23 @@ class TaskRepository(ContentRepository):
         """Получить список задач из категорий на которые подписан пользователь."""
         return await self._session.scalars(select(Task).options(joinedload(Task.category)))
 
-    async def get_tasks_limit_for_user(self, limit: int, offset: int, user_telegram_id: int) -> list[Task]:
+    async def get_tasks_limit_for_user(self, limit: int, offset: int, user: User) -> list[Task]:
         """Получить limit-выборку из списка всех задач пользователя."""
-        user_id = await self._session.scalar((select(User.id).where(User.telegram_id == user_telegram_id)))
         return await self._session.scalars(
             select(Task)
             .join(Category)
             .options(joinedload(Task.category))
-            .where(and_(Category.users.any(id=user_id)), Task.is_archived == false())
+            .where(Category.users.any(id=user.id))
+            .where(Task.is_archived == false())
             .limit(limit)
             .offset(offset)
         )
 
-    async def get_user_tasks_count(self, user_telegram_id: int) -> int:
+    async def get_user_tasks_count(self, user: User) -> int:
         """Получить общее количество задач для пользователя."""
-        user_id = await self._session.scalar((select(User.id).where(User.telegram_id == user_telegram_id)))
-        tasks = await self._session.scalars(
-            select(Task)
+        return await self._session.scalar(select(
+            func.count(Task.id))
             .join(Category)
-            .options(joinedload(Task.category))
-            .where(and_(Category.users.any(id=user_id)), Task.is_archived == false())
+            .where(Category.users.any(id=user.id))
+            .where(Task.is_archived == false())
         )
-        return len(tasks.all())
