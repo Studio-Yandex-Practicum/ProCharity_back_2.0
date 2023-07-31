abb2748f7578103f528e4e60f5f87f754c11ec88
Igor Sidorov <isidorov@wiley.com>
2023-07-31 22:34:38 +0300

    fix tag

--- src/api/router.py
+++ src/api/router.py
@@ -5,7 +5,7 @@ from src.settings import settings
 
 api_router = APIRouter(prefix=settings.ROOT_PATH)
 api_router.include_router(category_router, prefix="/categories", tags=["Categories"])
-api_router.include_router(analytic_router, prefix="/analytics", tags=["Statistic"])
+api_router.include_router(analytic_router, prefix="/analytics", tags=["Analytic"])
 api_router.include_router(task_router, prefix="/tasks", tags=["Tasks"])
 api_router.include_router(notification_router, prefix="/messages", tags=["Messages"])
 api_router.include_router(site_user_router, prefix="/external_user_registration", tags=["ExternalSiteUser"])
