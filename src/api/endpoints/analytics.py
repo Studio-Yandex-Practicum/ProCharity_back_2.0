ac5bde96823f5c162fd43eb03895d9ab0c68f078
Igor Sidorov <isidorov@wiley.com>
2023-07-31 22:31:39 +0300

    clean unused

--- src/api/endpoints/analytics.py
+++ src/api/endpoints/analytics.py
@@ -9,15 +9,5 @@ analytic_router = APIRouter()
 
 @analytic_router.get("/", description="Возращает количество пользователей.")
 async def get_users(
-    tasks: list[TaskRequest],
-    task_service: TaskService = Depends(),
-    telegram_notification_service: TelegramNotificationService = Depends(),
 ) -> Users:
     pass
-    # users = db_session.query(User.has_mailing, User.banned).all()
-    # number_users = len(users)
-    # # user[0] - has_mailing, user[1] - banned
-    # subscribed_users = len([user for user in users if user[0] and not user[1]])
-    # not_subscribed_users = len([user for user in users if not user[0] and not user[1]])
-    # banned_users = len([user for user in users if user[1]])
-    # return Users
