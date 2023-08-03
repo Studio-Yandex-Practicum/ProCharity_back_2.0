14ec164944ea3d9f8d1ce6d65fb02a5299ae366f
Igor Sidorov <isidorov@wiley.com>
2023-08-03 22:58:36 +0300

    wip

--- src/api/endpoints/analytics.py
+++ src/api/endpoints/analytics.py
@@ -1,13 +1,17 @@
 from fastapi import APIRouter, Depends
 
 from src.api.schemas import Statistic
-from src.api.services.analitics import get_number_users_statistic
+from src.api.services.analitics import AnalyticsService
 
 analytic_router = APIRouter()
 
 
-@analytic_router.get("/", description="Возращает статистику сервиса.")
-async def get_statistics() -> Statistic:
+@analytic_router.get("/", response_model=Statistic,
+                     description="Возращает статистику сервиса.")
+async def get_statistics(
+    analytic_service: AnalyticsService = Depends()
+) -> Statistic:
+    users_number = await analytic_service.get_users_number()
     statistics = Statistic
-    statistics.number_users = get_number_users_statistic()
+    statistics.number_users = users_number
     return statistics
