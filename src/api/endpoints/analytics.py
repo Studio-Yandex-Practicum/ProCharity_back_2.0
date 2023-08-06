b21bf8b8beb424a32a6b4326cfd1c67eecfab4f2
Igor Sidorov <isidorov@wiley.com>
2023-08-06 20:25:58 +0300

    wip

--- src/api/endpoints/analytics.py
+++ src/api/endpoints/analytics.py
@@ -6,6 +6,6 @@ from src.api.services.analitics import AnalyticsService
 analytic_router = APIRouter()
 
 
-@analytic_router.get("/", response_model=Analytic, description="Возращает статистику сервиса.")
+@analytic_router.get("/", description="Возращает статистику сервиса.")
 async def get_statistics(analytic_service: AnalyticsService = Depends()) -> Analytic:
     return await analytic_service.get_user_number()
--- src/api/services/analitics.py
+++ src/api/services/analitics.py
@@ -4,19 +4,17 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from src.api.schemas import Analytic
 from src.core.db import get_session
 from src.core.db.models import User
-from src.core.db.repository.base import AbstractRepository
+from src.core.db.repository import UserRepository
 
 
 class AnalyticsService:
     """Сервис для работы с моделью Analitics."""
 
-    # def __init__(self, repository: AbstractRepository = Depends()) -> None:
-    #     self._repository: AbstractRepository = repository
+    def __init__(self, user_repository: UserRepository = Depends()) -> None:
+        self._user_repository: UserRepository = user_repository
         # self._session: AsyncSession = session
 
     async def get_user_number(self) -> None:
-        analytic = Analytic
-        print("AbstractRepository", type(self._repository.count_all(User)), self._repository.count_all(User))
-        analytic.number_users = int(self._repository.count_all(User))
+        analytic = Analytic()
+        analytic.number_users = int(self._user_repository.count_all(User))
         return analytic
-        # pass
