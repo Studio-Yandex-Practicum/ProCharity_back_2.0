41a897e7995618e91333cd762a4cf216c38b8619
Артем Баландин <tom9981@yandex.ru>
2023-08-11 10:16:16 +0500

    feature/add_webhook_ERRORS

--- src/api/endpoints/telegram_webhook.py
+++ src/api/endpoints/telegram_webhook.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Request
-from telegram import Update
+# from telegram import Update
 
 from src.settings import settings
 
@@ -15,7 +15,7 @@ if settings.BOT_WEBHOOK_MODE:
     async def get_telegram_bot_updates(request: Request):
         """Получение обновлений telegram в режиме работы бота webhook."""
         print(f'request_scope: {request.scope}')  # Печатает словарь
-        # request_json_data = await request.json()
+        # request_json_data = await request.json() # -> !!! JSONDecodeError
 
         # print(f'request_json_data: {request_json_data}')
         # --->>> !!! JSONDecodeError: Expecting value: line 1 column 1 (char 0)
