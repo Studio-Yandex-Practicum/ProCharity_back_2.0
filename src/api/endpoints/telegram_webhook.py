863a52e7ea8dc21fedbb6162d427a988663f5b03
Артем Баландин <tom9981@yandex.ru>
2023-08-12 20:49:28 +0500

    feature/add_webhook_ERRORS_1208

--- src/api/endpoints/telegram_webhook.py
+++ src/api/endpoints/telegram_webhook.py
@@ -8,17 +8,17 @@ from src.settings import settings
 telegram_webhook_router = APIRouter()
 
 
-if settings.BOT_WEBHOOK_MODE:
-    print(f"BOT_WEBHOOK_MODE: {settings.BOT_WEBHOOK_MODE}")
+# if settings.BOT_WEBHOOK_MODE:
+#     print(f"BOT_WEBHOOK_MODE: {settings.BOT_WEBHOOK_MODE}")
 
-    @telegram_webhook_router.post(
-        "/webhook",
-        description="Получить обновления telegram.",
-    )
-    async def get_telegram_bot_updates(request: Request):
-        """Получение обновлений telegram в режиме работы бота webhook."""
-        print(f"request_scope: {request.scope}")  # Печатает словарь
-        request_json_data = await request.json()  # -> !!! Падает с JSONDecodeError
+@telegram_webhook_router.post(
+    "/webhook",
+    description="Получить обновления telegram.",
+)
+async def get_telegram_bot_updates(request: Request):
+    """Получение обновлений telegram в режиме работы бота webhook."""
+    print(f"request_scope: {request.scope}")  # Печатает словарь
+    request_json_data = await request.json()  # -> !!! Падает с JSONDecodeError
 
-        print(f"request_json_data: {request_json_data}")
-        # --->>> !!! JSONDecodeError: Expecting value: line 1 column 1 (char 0)
+    print(f"request_json_data: {request_json_data}")
+    #--->>> !!! JSONDecodeError: Expecting value: line 1 column 1 (char 0)
