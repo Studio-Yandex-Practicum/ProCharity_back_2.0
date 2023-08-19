b3fed45adfc04ba60610e204448cc99a0f9a38ce
vadim <vadimbor93@mail.ru>
2023-08-19 19:22:46 +0300

    закоментирован код для будущего удаления

--- src/application.py
+++ src/application.py
@@ -1,54 +1,76 @@
-from asgi_correlation_id import CorrelationIdMiddleware
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi.staticfiles import StaticFiles
+# from asgi_correlation_id import CorrelationIdMiddleware
+# from fastapi import FastAPI
+# from fastapi.middleware.cors import CORSMiddleware
+# from fastapi.staticfiles import StaticFiles
 
-from src.api.router import api_router
-from src.bot.bot import shutdown_bot, startup_bot
-from src.core.logging.middleware import LoggingMiddleware
-from src.core.logging.setup import setup_logging
-from src.core.utils import set_ngrok
-from src.settings import settings
+# from src.api.router import api_router
+# from src.bot.bot import shutdown_bot, startup_bot
+# from src.core.logging.middleware import LoggingMiddleware
+# from src.core.logging.setup import setup_logging
+# from src.core.utils import set_ngrok
+# from src.settings import settings
 
 
-def create_app(run_bot: bool = True) -> FastAPI:
-    app = FastAPI(
-        debug=settings.DEBUG,
-    )
-    origins = ["*"]
+# def create_app(run_bot: bool = True) -> FastAPI:
+#     app = FastAPI(
+#         debug=settings.DEBUG,
+#     )
 
-    app.add_middleware(
-        CORSMiddleware,
-        allow_origins=origins,
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
-    )
+#     @app.on_event("startup")
+#     async def on_startup():
+#         """Действия при запуске сервера."""
+#         if run_bot:
+#             app.state.bot_instance = await startup_bot()
+#         if settings.USE_NGROK:
+#             set_ngrok()
 
-    setup_logging()
-    app.add_middleware(LoggingMiddleware)
-    app.add_middleware(CorrelationIdMiddleware)
+#     @app.on_event("shutdown")
+#     async def on_shutdown():
+#         """Действия после остановки сервера."""
+#         if run_bot:
+#             await shutdown_bot(app.state.bot_instance)
 
-    app.include_router(api_router)
-    if settings.DEBUG:
-        app.mount(
-            "/static",
-            StaticFiles(directory=settings.STATIC_DIR, html=True),
-            name="static",
-        )
+#     return app
 
-    @app.on_event("startup")
-    async def on_startup():
-        """Действия при запуске сервера."""
-        if run_bot:
-            app.state.bot_instance = await startup_bot()
-        if settings.USE_NGROK:
-            set_ngrok()
+# бекап:
+# def create_app(run_bot: bool = True) -> FastAPI:
+#     app = FastAPI(
+#         debug=settings.DEBUG,
+#     )
+#     origins = ["*"]
 
-    @app.on_event("shutdown")
-    async def on_shutdown():
-        """Действия после остановки сервера."""
-        if run_bot:
-            await shutdown_bot(app.state.bot_instance)
+#     app.add_middleware(
+#         CORSMiddleware,
+#         allow_origins=origins,
+#         allow_credentials=True,
+#         allow_methods=["*"],
+#         allow_headers=["*"],
+#     )
 
-    return app
+#     setup_logging()
+#     app.add_middleware(LoggingMiddleware)
+#     app.add_middleware(CorrelationIdMiddleware)
+
+#     app.include_router(api_router)
+#     if settings.DEBUG:
+#         app.mount(
+#             "/static",
+#             StaticFiles(directory=settings.STATIC_DIR, html=True),
+#             name="static",
+#         )
+
+#     @app.on_event("startup")
+#     async def on_startup():
+#         """Действия при запуске сервера."""
+#         if run_bot:
+#             app.state.bot_instance = await startup_bot()
+#         if settings.USE_NGROK:
+#             set_ngrok()
+
+#     @app.on_event("shutdown")
+#     async def on_shutdown():
+#         """Действия после остановки сервера."""
+#         if run_bot:
+#             await shutdown_bot(app.state.bot_instance)
+
+#     return app
--- src/run_api.py
+++ src/run_api.py
@@ -1,12 +1,27 @@
-import uvicorn
+# import uvicorn
 
-from application import create_app
-
-
-def start_api():
-    app = create_app()
-    uvicorn.run(app, host="localhost", port=8000)
+# from application import create_app
 
 
-if __name__ == "__main__":
-    start_api()
+# def start_api():
+#     app = create_app()
+#     uvicorn.run(app, host="localhost", port=8000)
+
+
+# if __name__ == "__main__":
+#     start_api()
+
+
+# бэкап
+# import uvicorn
+
+# from application import create_app
+
+
+# def start_api():
+#     app = create_app()
+#     uvicorn.run(app, host="localhost", port=8000)
+
+
+# if __name__ == "__main__":
+#     start_api()
