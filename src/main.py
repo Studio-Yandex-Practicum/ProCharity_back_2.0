c4c0e01fc13d43abaf3857e6af336c861fda56f7
vadim <vadimbor93@mail.ru>
2023-08-23 17:14:29 +0300

    добавлены параметры из DI Container

--- src/main.py
+++ src/main.py
@@ -17,15 +17,23 @@ def main(run_bot: bool = True) -> FastAPI:
     @fastapi_app.on_event("startup")
     async def on_startup():
         """Действия при запуске сервера."""
-        if run_bot:
-            fastapi_app.state.bot_instance = await startup_bot(bot)
         if container.settings.provided.USE_NGROK is True:
             set_ngrok()
+        if run_bot:
+            fastapi_app.state.bot_instance = await startup_bot(
+                bot=bot,
+                BOT_WEBHOOK_MODE=container.settings.provided.BOT_WEBHOOK_MODE,
+                telegram_webhook_url=container.settings.provided.telegram_webhook_url,
+                SECRET_KEY=container.settings.provided.SECRET_KEY,
+            )
 
     @fastapi_app.on_event("shutdown")
     async def on_shutdown():
         """Действия после остановки сервера."""
         if run_bot:
-            await shutdown_bot(fastapi_app.state.bot_instance)
+            await shutdown_bot(
+                fastapi_app.state.bot_instance,
+                BOT_WEBHOOK_MODE=container.settings.provided.BOT_WEBHOOK_MODE
+            )
 
     return fastapi_app
