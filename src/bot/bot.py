7b1294e4b6a47031c4ef1c35d2b13295ae8d7396
vadim <vadimbor93@mail.ru>
2023-08-23 17:12:59 +0300

    settings заменены на параметры из DI контейнера

--- src/bot/bot.py
+++ src/bot/bot.py
@@ -2,22 +2,25 @@ import logging
 
 from telegram.ext import AIORateLimiter, Application
 
-from src.settings import settings
-
 
 def create_bot(bot_token) -> Application:
     bot = Application.builder().token(bot_token).rate_limiter(AIORateLimiter()).build()
     return bot
 
 
-async def start_bot(bot: Application) -> Application:
+async def start_bot(
+        bot: Application,
+        BOT_WEBHOOK_MODE: bool,
+        telegram_webhook_url: str,
+        SECRET_KEY: str
+) -> Application:
     """Запуск бота в `Background` режиме."""
     await bot.initialize()
-    if settings.BOT_WEBHOOK_MODE:
+    if BOT_WEBHOOK_MODE is True:
         bot.updater = None
         await bot.bot.set_webhook(
-            url=settings.telegram_webhook_url,
-            secret_token=settings.SECRET_KEY,
+            url=telegram_webhook_url,
+            secret_token=SECRET_KEY,
         )
     else:
         await bot.updater.start_polling()  # type: ignore
@@ -26,13 +29,18 @@ async def start_bot(bot: Application) -> Application:
     return bot
 
 
-async def startup_bot(bot: Application) -> Application:
-    bot_instance = await start_bot(bot)
+async def startup_bot(
+        bot: Application,
+        BOT_WEBHOOK_MODE: bool,
+        telegram_webhook_url: str,
+        SECRET_KEY: str
+) -> Application:
+    bot_instance = await start_bot(bot, BOT_WEBHOOK_MODE, telegram_webhook_url, SECRET_KEY)
     return bot_instance
 
 
-async def shutdown_bot(bot_instance):
-    if not settings.BOT_WEBHOOK_MODE:
+async def shutdown_bot(bot_instance, BOT_WEBHOOK_MODE: bool):
+    if BOT_WEBHOOK_MODE is True:
         await bot_instance.updater.stop()
     await bot_instance.stop()
     await bot_instance.shutdown()
