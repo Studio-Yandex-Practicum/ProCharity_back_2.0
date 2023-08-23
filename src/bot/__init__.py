5f038a9b308d717d996b7513cc2295f6d99e52b3
vadim <vadimbor93@mail.ru>
2023-08-23 14:03:37 +0300

    добавлена функция init_bot

--- src/bot/__init__.py
+++ src/bot/__init__.py
@@ -1,6 +1,22 @@
+from telegram.ext import Application, TypeHandler
+
 from .bot import create_bot, start_bot
+from src.bot.handlers import categories, feedback_form, menu, registration, tasks
+from telegram import Update
+from src.core.logging.utils import logging_updates
 
 __all__ = (
     "start_bot",
     "create_bot",
 )
+
+
+def init_bot(telegram_bot: Application) -> Application:
+    """Инициализация телеграм бота."""
+    registration.registration_handlers(telegram_bot)
+    categories.registration_handlers(telegram_bot)
+    tasks.registration_handlers(telegram_bot)
+    menu.registration_handlers(telegram_bot)
+    feedback_form.registration_handlers(telegram_bot)
+    telegram_bot.add_handler(TypeHandler(Update, logging_updates))
+    return telegram_bot
