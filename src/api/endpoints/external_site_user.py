033b61ed521371a067cd4fb89a38357481d629ad
SlavyaN009 <slav009@yandex.ru>
2023-08-27 17:37:43 +0300

    site user inject

--- src/api/endpoints/external_site_user.py
+++ src/api/endpoints/external_site_user.py
@@ -1,13 +1,16 @@
+from dependency_injector.wiring import Provide, inject
 from fastapi import APIRouter, Depends
 
 from src.api.schemas import ExternalSiteUserRequest
 from src.api.services import ExternalSiteUserService
+from src.depends import Container
 
 site_user_router = APIRouter()
 
 
 @site_user_router.post("/", description="Актуализирует пользователя с сайта ProCharity.")
+@inject
 async def external_user_registration(
-    site_user: ExternalSiteUserRequest, site_user_service: ExternalSiteUserService = Depends()
+    site_user: ExternalSiteUserRequest, site_user_service: ExternalSiteUserService = Depends(Provide[Container.site_user_service])
 ) -> None:
     await site_user_service.register(site_user)
--- src/depends.py
+++ src/depends.py
@@ -2,8 +2,10 @@ from dependency_injector import containers, providers
 from fastapi import FastAPI
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 
+from src.api.services import ExternalSiteUserService
 from src.bot.bot import create_bot
 from src.core.db import get_session
+from src.core.db.repository.external_site_user import ExternalSiteUserRepository
 from src.settings import get_settings
 
 
@@ -21,3 +23,6 @@ class Container(containers.DeclarativeContainer):
     # Applications
     fastapi_app = providers.Singleton(FastAPI, debug=settings.provided.DEBUG)
     telegram_bot = providers.Singleton(create_bot, bot_token=settings.provided.BOT_TOKEN)
+
+    site_user_repository = providers.Factory(ExternalSiteUserRepository, session=session)
+    site_user_service = providers.Factory(ExternalSiteUserService, site_user_repository=site_user_repository)
