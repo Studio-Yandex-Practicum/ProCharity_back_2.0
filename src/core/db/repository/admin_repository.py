868e340a927d9b4e20f3bfd3100d4fcbec64ba6e
Konstantin Korneev <konstantinkorneev@MacBook-Air-Konstantin.local>
2023-08-29 15:18:06 +0300

    add_jwt_token

--- src/core/db/repository/admin_repository.py
+++ src/core/db/repository/admin_repository.py
@@ -1,6 +1,8 @@
+from fastapi import Depends
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from src.core.db import get_session
 from src.core.db.models import AdminUser
 from src.core.db.repository.base import AbstractRepository
 
@@ -8,8 +10,8 @@ from src.core.db.repository.base import AbstractRepository
 class AdminUserRepository(AbstractRepository):
     """Репозиторий для работы с моделью AdminUser."""
 
-    def __init__(self, session: AsyncSession) -> None:
-        self._session: AsyncSession = session
+    def __init__(self, session: AsyncSession = Depends(get_session)) -> None:
+        super().__init__(session, AdminUser)
 
     async def get_by_email(self, email: str) -> AdminUser | None:
         """Возвращает пользователя (или None) по email."""
