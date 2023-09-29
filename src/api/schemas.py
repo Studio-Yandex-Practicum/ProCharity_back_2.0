d602e6f9bea56ba647fd3e89317531a32a0cf911
Борис Бескровный <beskrovniibv@yandex.ru>
2023-09-29 12:53:15 +0500

    fixed TaskRequest schema according to Task model

--- src/api/schemas.py
+++ src/api/schemas.py
@@ -1,10 +1,10 @@
 import urllib
-from datetime import date
+from datetime import date, datetime
 
 from pydantic import BaseModel, Extra, Field, NonNegativeInt, StrictStr, field_validator, root_validator
 from typing_extensions import NotRequired, TypedDict
 
-from src.api.constants import DATE_FORMAT
+from src.api.constants import DATE_FORMAT, DATE_FORMAT_FOR_TASK_SCHEMA
 from src.core.db.models import ExternalSiteUser
 from src.core.enums import TelegramNotificationUsersGroups
 
@@ -75,23 +75,29 @@ class CategoryResponse(ResponseBase):
 class TaskRequest(RequestBase):
     """Класс модели запроса для Task."""
 
-    id: NonNegativeInt = Field(..., ge=1, lt=10, example=1, description="Уникальный идентификатор задачи.")
+    id: NonNegativeInt = Field(..., ge=1, example=1, description="Уникальный идентификатор задачи.")
     title: StrictStr = Field(..., example="Task Title", description="Название задачи.")
     name_organization: StrictStr = Field(
         ..., example="My Organization", description="Название организации, оставившей задачу."
     )
-    deadline: date = Field(
-        ..., format=DATE_FORMAT, example="31-12-2025", description="Время, до которого нужно выполнить задачу."
-    )
+    deadline: date = Field(..., example="31.12.2025", description="Время, до которого нужно выполнить задачу.")
     category_id: NonNegativeInt = Field(
-        ..., example=1, description="Показывает, к какой дочерней категории относится задача."
+        ..., example=1, description="ID дочерней категории, к которой относится задача."
     )
+    category: StrictStr = Field(..., example="Категория", description="Дочерняя категории, к которой относится задача.")
     bonus: NonNegativeInt = Field(..., ge=1, lt=10, example=5, description="Величина бонуса за выполнение задачи.")
     location: StrictStr = Field(..., example="My Location", description="Локация, в которой находится заказчик задачи.")
     link: StrictStr = Field(..., example="https://example.com", description="Ссылка на сайт, где размещена задача.")
     description: StrictStr = Field(None, example="Task description", description="Описание задачи.")
 
+    @field_validator("deadline", mode="before")
+    def str_to_date(cls, v: object) -> object:
+        if isinstance(v, str):
+            return datetime.strptime(v, DATE_FORMAT_FOR_TASK_SCHEMA).date()
+        return v
+
     class Config:
+        extra = Extra.ignore
         json_schema_extra = {
             "example": {
                 "id": 1,
