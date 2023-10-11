74cf845bbf04959aa4ebe7f9534a7d3696d60dc5
Gribko Konstantin <czarkost@yandex.ru>
2023-10-11 13:44:24 +0300

    startup dependency injection

--- src/main.py
+++ src/main.py
@@ -1,28 +1,39 @@
+from dependency_injector.wiring import Provide
 from fastapi import FastAPI
+from telegram.ext import Application
 
 from src.bot import shutdown_bot, startup_bot
 from src.core.utils import set_ngrok
 from src.depends import Container
+from src.settings import Settings
+
+
+async def startup(
+    run_bot: bool,
+    fastapi_app: FastAPI = Provide[Container.fastapi_app],
+    bot: Application = Provide[Container.telegram_bot],
+    settings: Settings = Provide[Container.settings],
+):
+    if settings.USE_NGROK is True:
+        set_ngrok()
+    if run_bot:
+        fastapi_app.state.bot_instance = await startup_bot(
+            bot=bot,
+            bot_webhook_mode=settings.BOT_WEBHOOK_MODE,
+            telegram_webhook_url=settings.telegram_webhook_url,
+            secret_key=settings.SECRET_KEY,
+        )
 
 
 def main(run_bot: bool = True) -> FastAPI:
     container = Container()
     container.wire(packages=(__package__,))
     fastapi_app = container.fastapi_app()
-    bot = container.telegram_bot()
 
     @fastapi_app.on_event("startup")
     async def on_startup():
         """Действия при запуске сервера."""
-        if container.settings().USE_NGROK is True:
-            set_ngrok()
-        if run_bot:
-            fastapi_app.state.bot_instance = await startup_bot(
-                bot=bot,
-                bot_webhook_mode=container.settings().BOT_WEBHOOK_MODE,
-                telegram_webhook_url=container.settings().telegram_webhook_url,
-                secret_key=container.settings().SECRET_KEY,
-            )
+        await startup(run_bot)
 
     @fastapi_app.on_event("shutdown")
     async def on_shutdown():
