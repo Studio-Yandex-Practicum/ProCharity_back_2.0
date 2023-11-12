a172d05f9890428344dc70a97d27b3858b9caa50
Nikita Levushkin <therederickmind@gmail.com>
2023-11-12 21:18:33 +0300

    Добавил переменную __all__ в src/api/schemas/__init__.py

--- src/api/schemas/__init__.py
+++ src/api/schemas/__init__.py
@@ -1,10 +1,10 @@
-from .admin import AdminUserRequest  # noqa
-from .analytics import Analytic  # noqa
-from .base import RequestBase, ResponseBase  # noqa
-from .categories import CategoryRequest, CategoryResponse  # noqa
-from .external_site_user import ExternalSiteUser, ExternalSiteUserRequest  # noqa
-from .health_check import BotStatus, CommitStatus, DBStatus, HealthCheck  # noqa
-from .notification import (  # noqa
+from .admin import AdminUserRequest
+from .analytics import Analytic
+from .base import RequestBase, ResponseBase
+from .categories import CategoryRequest, CategoryResponse
+from .external_site_user import ExternalSiteUser, ExternalSiteUserRequest
+from .health_check import BotStatus, CommitStatus, DBStatus, HealthCheck
+from .notification import (
     ErrorsSending,
     FeedbackFormQueryParams,
     InfoRate,
@@ -14,4 +14,29 @@ from .notification import (  # noqa
     TelegramNotificationUsersGroups,
     TelegramNotificationUsersRequest,
 )
-from .tasks import TaskRequest, TaskResponse  # noqa
+from .tasks import TaskRequest, TaskResponse
+
+__all__ = (
+    AdminUserRequest,
+    Analytic,
+    RequestBase,
+    ResponseBase,
+    CategoryRequest,
+    CategoryResponse,
+    ExternalSiteUser,
+    ExternalSiteUserRequest,
+    BotStatus,
+    CommitStatus,
+    DBStatus,
+    HealthCheck,
+    ErrorsSending,
+    FeedbackFormQueryParams,
+    InfoRate,
+    Message,
+    MessageList,
+    TelegramNotificationRequest,
+    TelegramNotificationUsersGroups,
+    TelegramNotificationUsersRequest,
+    TaskRequest,
+    TaskResponse,
+)
