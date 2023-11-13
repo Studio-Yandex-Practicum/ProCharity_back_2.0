6bcf07c0e49e11bfc5837499ab51b07282875662
Nikita Levushkin <therederickmind@gmail.com>
2023-11-13 20:06:24 +0300

    Откатил свои изменения в src/depends.py. Перенес метод получения аналитики по задачам из src/api/services/analytics.py в src/api/endpoints/analytics.py

--- src/api/endpoints/analytics.py
+++ src/api/endpoints/analytics.py
@@ -1,8 +1,9 @@
 from dependency_injector.wiring import Provide, inject
 from fastapi import APIRouter, Depends
 
-from src.api.schemas import ActiveTasks, Analytic
+from src.api.schemas import ActiveTasks, Analytic, DBStatus
 from src.api.services.analytics import AnalyticsService
+from src.api.services.health_check import HealthCheckService
 from src.depends import Container
 
 analytic_router = APIRouter()
@@ -16,7 +17,11 @@ async def get_analytics(analytic_service: AnalyticsService = Depends(Provide[Con
 
 @inject
 async def get_active_tasks_analytic(
-    analytic_service: AnalyticsService = Depends(Provide[Container.analytic_service]),
+    health_check_service: HealthCheckService = Depends(Provide[Container.health_check_service]),
 ) -> ActiveTasks:
-    last_update, active_tasks = await analytic_service.get_count_active_tasks()
+    db_status: DBStatus = await health_check_service.check_db_connection()
+    last_update = db_status["last_update"]
+    if not (db_status["status"] or db_status["last_update"]):
+        last_update = "Unable to get last_update"
+    active_tasks = db_status["active_tasks"]
     return ActiveTasks(last_update=last_update, active_tasks=active_tasks)
--- src/api/services/analytics.py
+++ src/api/services/analytics.py
@@ -1,22 +1,11 @@
-from src.api.schemas import DBStatus
-from src.api.services.health_check import HealthCheckService
 from src.core.db.repository import UserRepository
 
 
 class AnalyticsService:
     """Сервис для работы с моделью Analytics."""
 
-    def __init__(self, user_repository: UserRepository, health_check_service: HealthCheckService) -> None:
+    def __init__(self, user_repository: UserRepository) -> None:
         self._user_repository: UserRepository = user_repository
-        self._health_check_service: HealthCheckService = health_check_service
 
     async def get_user_number(self) -> None:
         return await self._user_repository.count_all()
-
-    async def get_count_active_tasks(self) -> None:
-        db_status: DBStatus = await self._health_check_service.check_db_connection()
-        last_update = db_status["last_update"]
-        if not (db_status["status"] or db_status["last_update"]):
-            last_update = "Unable to get last_update"
-        active_tasks = db_status["active_tasks"]
-        return last_update, active_tasks
