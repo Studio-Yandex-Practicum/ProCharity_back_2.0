9b051c7930a221bde2d11111ff6c689614c40487
Андрей Филатов <filatov.phoenix@yandex.ru>
2023-11-16 22:26:41 +0300

    Изменение имени константы DAYS_NUMBER

--- src/api/services/analytics.py
+++ src/api/services/analytics.py
@@ -1,6 +1,6 @@
 from datetime import timedelta
 
-from src.api.constants import DAYS_NUMBER
+from src.api.constants import DAYS_NUMBER_FOR_USERS_STATISTIC
 from src.core.db.repository import UnsubscribeReasonRepository, UserRepository
 
 
@@ -19,19 +19,19 @@ class AnalyticsService:
         return await self._user_repository.count_all()
 
     async def get_added_users_statistic(self, date_limit) -> dict[str, int]:
-        date_begin = date_limit - timedelta(days=DAYS_NUMBER - 1)
+        date_begin = date_limit - timedelta(days=DAYS_NUMBER_FOR_USERS_STATISTIC - 1)
         added_users = await self._user_repository.get_statistics_by_days(date_begin, date_limit, "created_at")
         return added_users
 
     async def get_added_external_users_statistic(self, date_limit) -> dict[str, int]:
-        date_begin = date_limit - timedelta(days=DAYS_NUMBER - 1)
+        date_begin = date_limit - timedelta(days=DAYS_NUMBER_FOR_USERS_STATISTIC - 1)
         added_external_users = await self._user_repository.get_statistics_by_days(
             date_begin, date_limit, "external_signup_date"
         )
         return added_external_users
 
     async def get_unsubscribed_users_statistic(self, date_limit) -> dict[str, int]:
-        date_begin = date_limit - timedelta(days=DAYS_NUMBER - 1)
+        date_begin = date_limit - timedelta(days=DAYS_NUMBER_FOR_USERS_STATISTIC - 1)
         users_unsubscribed = await self._unsubscribe_reason_repository.get_statistics_by_days(
             date_begin, date_limit, "created_at"
         )
