aebb7a20098e694817130fa729afb138114f24cf
Nikita Levushkin <therederickmind@gmail.com>
2023-11-16 20:23:18 +0300

    Добавление ActiveTasks в src/api/schemas/__init__.py

--- src/api/schemas/__init__.py
+++ src/api/schemas/__init__.py
@@ -1,5 +1,5 @@
 from .admin import AdminUserRequest
-from .analytics import Analytic
+from .analytics import ActiveTasks, Analytic
 from .base import RequestBase, ResponseBase
 from .categories import CategoryRequest, CategoryResponse
 from .external_site_user import ExternalSiteUser, ExternalSiteUserRequest
@@ -19,6 +19,7 @@ from .tasks import TaskRequest, TaskResponse
 __all__ = (
     "AdminUserRequest",
     "Analytic",
+    "ActiveTasks",
     "RequestBase",
     "ResponseBase",
     "CategoryRequest",
