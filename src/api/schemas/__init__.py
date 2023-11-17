16314c9bb9d105ae8355f154dc9f6775300b6bd8
Daniil Sukhochev <sushidze@gmail.com>
2023-11-17 12:05:43 +0000

    add ActiveTasks import to __init__.py

--- src/api/schemas/__init__.py
+++ src/api/schemas/__init__.py
@@ -1,5 +1,5 @@
 from .admin import AdminUserRequest
-from .analytics import Analytic
+from .analytics import Analytic, ActiveTasks
 from .base import RequestBase, ResponseBase
 from .categories import CategoryRequest, CategoryResponse
 from .external_site_user import ExternalSiteUser, ExternalSiteUserRequest
@@ -17,6 +17,7 @@ from .notification import (
 from .tasks import TaskRequest, TaskResponse
 
 __all__ = (
+    "ActiveTasks",
     "AdminUserRequest",
     "Analytic",
     "RequestBase",
