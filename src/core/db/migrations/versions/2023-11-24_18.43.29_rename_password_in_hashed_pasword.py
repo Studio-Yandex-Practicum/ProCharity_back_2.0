43c38522aacabbb92a56847ce669abc57b327349
Александр Батанов <pokertrend@yandex.ru>
2023-11-27 01:35:46 +0400

    добавлены поля is_active, is_superuser, is_verified

--- src/core/db/migrations/versions/2023-11-24_18.43.29_rename_password_in_hashed_pasword.py
+++ src/core/db/migrations/versions/2023-11-24_18.43.29_rename_password_in_hashed_pasword.py
@@ -26,6 +26,17 @@ def upgrade() -> None:
     if column_exists("admin_users", "password"):
         op.alter_column("admin_users", "password", new_column_name="hashed_password")
 
+    if not column_exists("admin_users", "is_superuser"):
+        op.add_column(
+            "admin_users", sa.Column("is_superuser", sa.Boolean(), server_default='false', nullable=False)
+        )
+        op.add_column(
+            "admin_users", sa.Column("is_verified", sa.Boolean(), server_default='false', nullable=False)
+        )
+        op.add_column(
+            "admin_users", sa.Column("is_active", sa.Boolean(), server_default='false', nullable=False)
+        )
+
 
 def downgrade() -> None:
     if column_exists("admin_users", "hashed_password"):
