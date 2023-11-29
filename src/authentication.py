f6932907e09d375cb5498ad6d01fc2af4ad5397e
Екатерина Атабекова <katerina19870405@yandex.ru>
2023-11-29 03:01:25 +0300

    adding refresh_token to the response during authorization

--- src/authentication.py
+++ src/authentication.py
@@ -1,14 +1,19 @@
-import structlog
 from typing import AsyncGenerator, Optional
 
+import structlog
 from fastapi import Depends, Request, Response
-from fastapi_users import BaseUserManager, FastAPIUsers, IntegerIDMixin, schemas
-from fastapi_users.authentication import (
-    AuthenticationBackend, BearerTransport, JWTStrategy
-)
+from fastapi.responses import JSONResponse
+from fastapi_users import BaseUserManager, FastAPIUsers, IntegerIDMixin, models, schemas
+from fastapi_users.authentication import AuthenticationBackend, BearerTransport, JWTStrategy
+from fastapi_users.authentication.strategy import Strategy
+from fastapi_users.authentication.transport import Transport
+from fastapi_users.schemas import model_dump
+from fastapi_users.types import DependencyCallable
 from fastapi_users_db_sqlalchemy import SQLAlchemyUserDatabase
+from pydantic import BaseModel
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
+
 from src.core.db.models import AdminUser, Base
 from src.settings import settings
 
@@ -40,17 +45,60 @@ class UserCreate(schemas.BaseUserCreate):
     pass
 
 
-bearer_transport = BearerTransport(tokenUrl="auth/login")
+class CustomBearerResponse(BaseModel):
+    access_token: str
+    refresh_token: str
+
+
+class CustomBearerTransport(BearerTransport):
+    async def get_login_response(self, token: str, refresh_token: str) -> Response:
+        bearer_response = CustomBearerResponse(
+            access_token=token,
+            refresh_token=refresh_token,
+        )
+        return JSONResponse(model_dump(bearer_response))
+
+
+class AuthenticationBackendRefresh(AuthenticationBackend):
+    def __init__(
+        self,
+        name: str,
+        transport: Transport,
+        get_strategy: DependencyCallable[Strategy[models.UP, models.ID]],
+        get_refresh_strategy: DependencyCallable[Strategy[models.UP, models.ID]],
+    ):
+        self.name = name
+        self.transport = transport
+        self.get_strategy = get_strategy
+        self.get_refresh_strategy = get_refresh_strategy
+
+    async def login(
+        self,
+        strategy: Strategy[models.UP, models.ID],
+        user: models.UP,
+    ) -> Response:
+        token = await strategy.write_token(user)
+        refresh_strategy = self.get_refresh_strategy()
+        refresh_token = await refresh_strategy.write_token(user)
+        return await self.transport.get_login_response(token=token, refresh_token=refresh_token)
+
+
+bearer_transport = CustomBearerTransport(tokenUrl="auth/login")
 
 
 def get_jwt_strategy() -> JWTStrategy:
     return JWTStrategy(secret=settings.SECRET_KEY, lifetime_seconds=3600)
 
 
-auth_backend = AuthenticationBackend(
+def get_refresh_jwt_strategy() -> JWTStrategy:
+    return JWTStrategy(secret=settings.SECRET_KEY, lifetime_seconds=7200)
+
+
+auth_backend = AuthenticationBackendRefresh(
     name="admin",
     transport=bearer_transport,
     get_strategy=get_jwt_strategy,
+    get_refresh_strategy=get_refresh_jwt_strategy,
 )
 
 
