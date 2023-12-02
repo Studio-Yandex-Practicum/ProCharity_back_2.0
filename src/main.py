1eb62e788b2aa4610e288ece84aea20416324317
= <=>
2023-12-02 17:44:15 +0300

    fix run

--- src/main.py
+++ src/main.py
@@ -7,9 +7,3 @@ def main(run_bot: bool = True) -> FastAPI:
     container = Container()
     container.wire(packages=(__package__,))
     return container.applications_container.fastapi_app(run_bot=run_bot)
-
-
-if __name__ == "__main__":
-    import uvicorn
-
-    uvicorn.run(main(), host="0.0.0.0", port=8000)
