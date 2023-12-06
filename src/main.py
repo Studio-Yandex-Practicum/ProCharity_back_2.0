97663874cc62f7d71bc0bdd43df6940c92cc53c2
= <=>
2023-12-06 18:43:44 +0300

    fhjasdhfj

--- src/main.py
+++ src/main.py
@@ -6,4 +6,3 @@ from src.core.depends import Container
 def main(run_bot: bool = True) -> FastAPI:
     container = Container()
     container.wire(packages=(__package__,))
-    return container.applications_container.fastapi_app(run_bot=run_bot)
