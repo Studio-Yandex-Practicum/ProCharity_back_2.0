10954f8123b365a261af4aa5100730a98a248b42
tWoAlex <thewoalex@gmail.com>
2023-12-21 16:07:05 +0500

    Added response gag with exception text

--- src/api/endpoints/health_check.py
+++ src/api/endpoints/health_check.py
@@ -16,10 +16,27 @@ health_check_router = APIRouter(dependencies=[Depends(check_header_contains_toke
 async def get_health_check(
     health_check_service: HealthCheckService = Depends(Provide[Container.api_services_container.health_check_service]),
 ) -> HealthCheck:
-    last_commit_data = await health_check_service.get_last_commit()
-    last_commit_data["git_tags"] = [str(tag) for tag in last_commit_data["git_tags"]]
-    return HealthCheck(
-        db=await health_check_service.check_db_connection(),
-        bot=await health_check_service.check_bot(),
-        git=last_commit_data,
-    )
+    try:
+        last_commit_data = await health_check_service.get_last_commit()
+        # last_commit_data["git_tags"] = [str(tag) for tag in last_commit_data["git_tags"]]
+        return HealthCheck(
+            db=await health_check_service.check_db_connection(),
+            bot=await health_check_service.check_bot(),
+            git=last_commit_data,
+        )
+    except Exception as exc:
+        return {
+            "db": {
+                "status": True,
+                "last_update": "placeholder",
+                "active_tasks": 666,
+                "db_connection_error": "placeholder",
+            },
+            "bot": {"status": True, "method": "placeholder", "url": "placeholder", "error": "placeholder"},
+            "git": {
+                "last_commit": "placeholder",
+                "commit_date": "placeholder",
+                "git_tags": ["1, 2, 3", "abc"],
+                "commit_error": str(exc),
+            },
+        }
