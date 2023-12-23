c4788d3d352d65364559689fbf96c3606f10cbc5
tWoAlex <thewoalex@gmail.com>
2023-12-23 16:21:15 +0500

    Removed forgotten unnecessary code

--- src/api/endpoints/health_check.py
+++ src/api/endpoints/health_check.py
@@ -16,10 +16,8 @@ health_check_router = APIRouter(dependencies=[Depends(check_header_contains_toke
 async def get_health_check(
     health_check_service: HealthCheckService = Depends(Provide[Container.api_services_container.health_check_service]),
 ) -> HealthCheck:
-    last_commit_data = await health_check_service.get_last_commit()
-    # last_commit_data["git_tags"] = [str(tag) for tag in last_commit_data["git_tags"]]
     return HealthCheck(
         db=await health_check_service.check_db_connection(),
         bot=await health_check_service.check_bot(),
-        git=last_commit_data,
+        git=await health_check_service.get_last_commit(),
     )
