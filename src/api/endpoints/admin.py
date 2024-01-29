3ed6411b93858234efb379cc79955a716dd8e2f5
Николай Ибраев <me1nik.nik@yandex.ru>
2024-01-29 19:36:00 +0300

    deleted_function

--- src/api/endpoints/admin.py
+++ src/api/endpoints/admin.py
@@ -5,10 +5,10 @@ from src.authentication import auth_backend, fastapi_users
 
 admin_user_router = APIRouter()
 
+auth_router = fastapi_users.get_auth_router(auth_backend)
+if auth_router is not None:
+    admin_user_router.include_router(auth_router, prefix="/auth", tags=["AdminUser"])
 
-admin_user_router.include_router(fastapi_users.get_auth_router(auth_backend), prefix="/auth", tags=["AdminUser"])
-admin_user_router.include_router(
-    fastapi_users.get_register_router(UserRead, UserCreate),
-    prefix="/auth",
-    tags=["auth"],
-)
+register_router = fastapi_users.get_register_router(UserRead, UserCreate)
+if register_router is not None:
+    admin_user_router.include_router(register_router, prefix="/auth", tags=["auth"])
