86d0add55fd7c79561fb160e686274ab64c10700
Kuznetsov Sergey <monteg179@yandex.ru>
2024-01-30 16:26:20 +0300

    fix: change ExternalSiteUser

--- src/core/db/models.py
+++ src/core/db/models.py
@@ -70,11 +70,12 @@ class ExternalSiteUser(Base):
     __tablename__ = "external_site_users"
 
     id_hash: Mapped[str] = mapped_column(String(256))
-    email: Mapped[str] = mapped_column(String(48), unique=True)
+    email: Mapped[str] = mapped_column(String(48), unique=True, nullable=True)
     first_name: Mapped[str] = mapped_column(String(64), nullable=True)
     last_name: Mapped[str] = mapped_column(String(64), nullable=True)
     specializations: Mapped[list[int]] = mapped_column(ARRAY(Integer), nullable=True)
     source: Mapped[str] = mapped_column(nullable=True)
+    external_id: Mapped[int] = mapped_column(nullable=True)
 
     def __repr__(self):
         return f"<SiteUser {self.id}>"
