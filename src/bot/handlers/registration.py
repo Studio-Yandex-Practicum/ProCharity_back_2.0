2945a1a3cf51044378121a6614f91bd5544d25cd
Евгений Москалянов <emos@inbox.ru>
2024-03-06 22:08:37 +0300

    Изменена логика регистрации пользователя согласно условиям задачи #395.

--- src/bot/handlers/registration.py
+++ src/bot/handlers/registration.py
@@ -1,15 +1,30 @@
 from dependency_injector.wiring import Provide, inject
 from telegram import Update
-from telegram.constants import ParseMode
-from telegram.ext import Application, CallbackQueryHandler, ChatMemberHandler, CommandHandler, ContextTypes
+from telegram.ext import Application, ChatMemberHandler, CommandHandler, ContextTypes
 
-from src.bot.constants import callback_data, commands
-from src.bot.keyboards import feedback_buttons, get_confirm_keyboard, get_start_keyboard
+from src.bot.constants import commands
+from src.bot.keyboards import get_start_keyboard
 from src.bot.services.external_site_user import ExternalSiteUserService
 from src.bot.services.user import UserService
-from src.bot.utils import delete_previous_message, get_connection_url
+from src.core.db.models import ExternalSiteUser
 from src.core.depends import Container
 from src.core.logging.utils import logger_decor
+
+
+async def determine_ext_user(
+    update: Update,
+    context: ContextTypes.DEFAULT_TYPE,
+    ext_user_service: ExternalSiteUserService,
+    user_service: UserService,
+) -> ExternalSiteUser | None:
+    if len(context.args) == 1:
+        return await ext_user_service.get_by_id_hash(context.args[0])
+    else:
+        user = await user_service.get_by_telegram_id(update.effective_user.id)
+        if user and user.external_id is not None:
+            return await ext_user_service.get_by_id(user.external_id)
+
+    return None
 
 
 @logger_decor
@@ -19,69 +34,28 @@ async def start_command(
     context: ContextTypes.DEFAULT_TYPE,
     ext_user_service: ExternalSiteUserService = Provide[Container.bot_services_container.bot_site_user_service],
     user_service: UserService = Provide[Container.bot_services_container.bot_user_service],
-    procharity_url: str = Provide[Container.settings.provided.PROCHARITY_URL],
 ):
+    ext_user = await determine_ext_user(update, context, ext_user_service, user_service)
+    if not ext_user:
+        return
+
     telegram_user = update.effective_user
-    ext_user, created = await ext_user_service.get_or_create(
-        id_hash=context.args[0] if context.args and len(context.args) == 1 else None
+    user = await user_service.register_user(
+        telegram_id=telegram_user.id,
+        username=telegram_user.username,
+        first_name=ext_user.first_name,
+        last_name=ext_user.last_name,
+        email=ext_user.email,
+        external_id=ext_user.id,
     )
-    if created or ext_user is None:
-        user = await user_service.register_user(
-            telegram_id=telegram_user.id,
-            username=telegram_user.username,
-            first_name=telegram_user.first_name,
-            last_name=telegram_user.last_name,
-            external_id=ext_user.id if ext_user is not None else None,
-        )
-        url_connect = get_connection_url(telegram_user.id)
-    elif ext_user is not None:
-        user = await user_service.register_user(
-            telegram_id=telegram_user.id,
-            username=telegram_user.username,
-            first_name=ext_user.first_name,
-            last_name=ext_user.last_name,
-            email=ext_user.email,
-            external_id=ext_user.id if ext_user is not None else None,
-        )
-        await user_service.set_categories_to_user(user.id, ext_user.specializations)
-        url_connect = get_connection_url(telegram_user.id, ext_user.id)
-    categories = await user_service.get_user_categories(telegram_user.id)
-    callback_data_on_start = commands.GREETING_REGISTERED_USER if categories else callback_data.CHANGE_CATEGORY
-    keyboard = await get_start_keyboard(callback_data_on_start=callback_data_on_start, url_for_connection=url_connect)
-    keyboard_feedback = await feedback_buttons(user)
+    await user_service.set_categories_to_user(user.id, ext_user.specializations)
+    keyboard = await get_start_keyboard()
     await context.bot.send_message(
         chat_id=telegram_user.id,
-        text="Привет! 👋 \n\n",
-        reply_markup=keyboard_feedback,
-    )
-    await context.bot.send_message(
-        chat_id=update.effective_user.id,
-        text=f'Я бот платформы интеллектуального волонтерства <a href="{procharity_url}">ProCharity</a>. '
-        "Буду держать тебя в курсе новых задач и помогу "
-        "оперативно связаться с командой поддержки.\n\n",
+        text="Авторизация прошла успешно!\n\n"
+        "Теперь оповещения будут приходить сюда. "
+        "Изменить настройку уведомлений можно в личном кабинете.\n\n",
         reply_markup=keyboard,
-        parse_mode=ParseMode.HTML,
-        disable_web_page_preview=True,
-    )
-
-
-@logger_decor
-@delete_previous_message
-async def confirm_chosen_categories(
-    update: Update,
-    context: ContextTypes.DEFAULT_TYPE,
-    user_service: UserService = Provide[Container.bot_services_container.bot_user_service],
-):
-    keyboard = get_confirm_keyboard()
-    categories = await user_service.get_user_categories(update.effective_user.id)
-    context.user_data["selected_categories"] = {category: None for category in categories}
-    text = ", ".join(categories.values())
-
-    await context.bot.send_message(
-        chat_id=update.effective_user.id,
-        text=f"Вот список твоих профессиональных компетенций: *{text}* Все верно?",
-        reply_markup=keyboard,
-        parse_mode=ParseMode.MARKDOWN,
     )
 
 
@@ -114,5 +88,4 @@ async def on_chat_member_update(
 
 def registration_handlers(app: Application):
     app.add_handler(CommandHandler(commands.START, start_command))
-    app.add_handler(CallbackQueryHandler(confirm_chosen_categories, pattern=commands.GREETING_REGISTERED_USER))
     app.add_handler(ChatMemberHandler(on_chat_member_update, chat_member_types=ChatMemberHandler.MY_CHAT_MEMBER))
--- src/bot/keyboards.py
+++ src/bot/keyboards.py
@@ -1,13 +1,7 @@
-from urllib.parse import urljoin
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
-from dependency_injector.wiring import Provide
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton, ReplyKeyboardMarkup, WebAppInfo
-
-from src.api.schemas import FeedbackFormQueryParams
 from src.bot.constants import callback_data, enum
 from src.core.db.models import Category, User
-from src.core.depends import Container
-from src.settings import settings
 
 MENU_KEYBOARD = [
     [InlineKeyboardButton("🔎 Посмотреть открытые задания", callback_data=callback_data.VIEW_TASKS)],
@@ -20,8 +14,7 @@ UNSUBSCRIBE_BUTTON = [
 SUBSCRIBE_BUTTON = [
     InlineKeyboardButton("▶️ Включить подписку на задания", callback_data=callback_data.JOB_SUBSCRIPTION)
 ]
-SUGGESTION_BUTTON_TITLE = "✉️ Отправить предложение/ошибку"
-QUESTION_BUTTON_TITLE = "❓ Задать вопрос"
+OPEN_MENU_BUTTON = [InlineKeyboardButton(text="Открыть меню", callback_data=callback_data.MENU)]
 
 
 async def get_checked_categories_keyboard(
@@ -75,46 +68,23 @@ async def get_menu_keyboard(user: User) -> InlineKeyboardMarkup:
     return InlineKeyboardMarkup(keyboard)
 
 
-async def feedback_buttons(user: User) -> ReplyKeyboardMarkup:
-    if hasattr(user, "email"):
-        email = user.email
-    else:
-        email = None
-    web_app = WebAppInfo(
-        url=urljoin(
-            settings.feedback_form_template_url,
-            FeedbackFormQueryParams(name=user.first_name, surname=user.last_name, email=email).as_url_query(),
-        )
-    )
-    keyboard = [
-        [KeyboardButton(QUESTION_BUTTON_TITLE, web_app=web_app)],
-        [KeyboardButton(SUGGESTION_BUTTON_TITLE, web_app=web_app)],
-    ]
-    return ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
-
-
 async def get_back_menu() -> InlineKeyboardMarkup:
     keyboard = [[InlineKeyboardButton(text="Вернуться в меню", callback_data=callback_data.MENU)]]
     return InlineKeyboardMarkup(keyboard)
 
 
-async def get_start_keyboard(
-    callback_data_on_start: str,
-    url_for_connection: str,
-    procharity_url: str = Provide[Container.settings.provided.PROCHARITY_URL],
-) -> InlineKeyboardMarkup:
+async def get_start_keyboard() -> InlineKeyboardMarkup:
     keyboard = [
-        [InlineKeyboardButton("Начнём", callback_data=callback_data_on_start)],
-        [InlineKeyboardButton("Перейти на сайт ProCharity", url=procharity_url)],
-        [InlineKeyboardButton("Связать аккаунт с ботом", url=url_for_connection)],
+        [InlineKeyboardButton("Перепроверить компетенции", callback_data=callback_data.CONFIRM_CATEGORIES)],
+        OPEN_MENU_BUTTON,
     ]
     return InlineKeyboardMarkup(keyboard)
 
 
 async def get_open_tasks_and_menu_keyboard() -> InlineKeyboardMarkup:
     keyboard = [
-        [InlineKeyboardButton("Посмотреть открытые задачи", callback_data=callback_data.VIEW_TASKS)],
-        [InlineKeyboardButton("Открыть меню", callback_data=callback_data.MENU)],
+        [InlineKeyboardButton("Посмотреть актуальные задания", callback_data=callback_data.VIEW_TASKS)],
+        OPEN_MENU_BUTTON,
     ]
     return InlineKeyboardMarkup(keyboard)
 
@@ -122,15 +92,7 @@ async def get_open_tasks_and_menu_keyboard() -> InlineKeyboardMarkup:
 async def view_more_tasks_keyboard() -> InlineKeyboardMarkup:
     keyboard = [
         [InlineKeyboardButton(text="Показать ещё задания", callback_data=callback_data.VIEW_TASKS)],
-        [InlineKeyboardButton(text="Открыть меню", callback_data=callback_data.MENU)],
-    ]
-    return InlineKeyboardMarkup(keyboard)
-
-
-def get_confirm_keyboard() -> InlineKeyboardMarkup:
-    keyboard = [
-        [InlineKeyboardButton("Да", callback_data=callback_data.CONFIRM_CATEGORIES)],
-        [InlineKeyboardButton("Нет, хочу изменить", callback_data=callback_data.CHANGE_CATEGORY)],
+        OPEN_MENU_BUTTON,
     ]
     return InlineKeyboardMarkup(keyboard)
 
