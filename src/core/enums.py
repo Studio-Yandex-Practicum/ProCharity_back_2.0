d63dd91f29942062c46435b1034b1d40874e3fc6
Евгений Москалянов <emos@inbox.ru>
2024-04-05 21:47:44 +0300

    Расширил докстринг.

--- src/core/enums.py
+++ src/core/enums.py
@@ -2,7 +2,7 @@ from enum import StrEnum
 
 
 class TelegramNotificationUsersGroups(StrEnum):
-    """Класс с доступными категориями пользователелей,
+    """Класс с доступными категориями пользователей,
     которым будет отправлено сообщение"""
 
     ALL = "all"
@@ -11,7 +11,11 @@ class TelegramNotificationUsersGroups(StrEnum):
 
 
 class UserRoles(StrEnum):
-    """Роли пользователя в системе."""
+    """Роли пользователя в системе.
+
+    Значения членов перечисления - строки длиной не более
+    src.core.db.models.MAX_USER_ROLE_NAME_LENGTH.
+    """
 
     FUND = "fund"
     VOLUNTEER = "volunteer"
