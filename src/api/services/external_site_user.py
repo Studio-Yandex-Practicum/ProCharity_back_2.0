3bbb7d7611547aa86fb35b2b8a51d02a23c84b76
Евгений Аборкин <newtowngenek@gmail.com>
2024-04-12 18:19:16 +0300

    fix/site_user.user

--- src/api/services/external_site_user.py
+++ src/api/services/external_site_user.py
@@ -20,24 +20,22 @@ class ExternalSiteUserService:
 
     async def register(self, site_user_schema: ExternalSiteVolunteerRequest | ExternalSiteFundRequest) -> None:
         site_user = await self._site_user_repository.get_by_id_hash(site_user_schema.id_hash)
+        user = site_user.user
 
         if site_user:
             await self._site_user_repository.update(site_user.id, site_user_schema.to_orm())
         else:
             await self._site_user_repository.create(site_user_schema.to_orm())
 
-        if site_user.user:
-            site_user.user.email = site_user_schema.email
-            site_user.user.first_name = site_user_schema.first_name
-            site_user.user.last_name = site_user_schema.last_name
+        if user:
+            user.email = site_user.email
+            user.first_name = site_user.first_name
+            user.last_name = site_user.last_name
+            user.role = site_user.role
 
             if site_user.role == UserRoles.VOLUNTEER:
-                site_user.user.role = UserRoles.VOLUNTEER
-                await self._user_repository.set_categories_to_user(site_user.user.id, site_user_schema.specializations)
+                await self._user_repository.set_categories_to_user(user.id, site_user_schema.specializations)
             else:
-                site_user.user.role = UserRoles.FUND
-                await self._user_repository.delete_all_categorys_from_user(site_user.user)
+                await self._user_repository.delete_all_categories_from_user(user)
 
-                site_user.specializations = None
-
-            await self._user_repository.update(site_user.user.id, site_user.user)
+            await self._user_repository.update(user.id, user)
