f5fd91d12190ff0a801dd3d8e678dcc8bb6d9d9b
Евгений Москалянов <emos@inbox.ru>
2024-04-16 22:48:24 +0300

    Рефакторинг FeedbackFormQueryParams и TaskInfoPageQueryParams. Общий метод as_url_query вынесен в базовый класс.

--- src/bot/schemas/web_app_query_params.py
+++ src/bot/schemas/web_app_query_params.py
@@ -1,12 +1,18 @@
 import urllib.parse
+from datetime import date
 
-from pydantic import BaseModel  # , Field
-
-from src.api.schemas.tasks import TaskRequest
+from pydantic import BaseModel, NonNegativeInt, StrictFloat
 
 
-class FeedbackFormQueryParams(BaseModel):
-    """Класс формирования параметров запроса для формы обратной связи."""
+class QueryParams(BaseModel):
+    """Параметры запроса."""
+
+    def as_url_query(self):
+        return f"?{urllib.parse.urlencode(self.model_dump(exclude_none=True))}"
+
+
+class FeedbackFormQueryParams(QueryParams):
+    """Параметры запроса для формы обратной связи."""
 
     external_id: int | None
     telegram_link: str | None
@@ -14,14 +20,23 @@ class FeedbackFormQueryParams(BaseModel):
     surname: str | None
     email: str | None
 
-    def as_url_query(self):
-        return f"?{urllib.parse.urlencode(self.model_dump(exclude_none=True))}"
 
+class TaskInfoPageQueryParams(QueryParams):
+    """Параметры запроса для страницы с информацией о задании."""
 
-class TaskInfoPageQueryParams(TaskRequest):
-    """Класс формирования параметров запроса для страницы с информацией о задании."""
-
-    category: str
-
-    def as_url_query(self):
-        return f"?{urllib.parse.urlencode(self.model_dump(exclude_none=True))}"
+    id: NonNegativeInt
+    title: str
+    name_organization: str | None
+    legal_address: str | None
+    fund_city: str | None
+    fund_rating: StrictFloat | None
+    fund_site: str | None
+    yb_link: str | None
+    vk_link: str | None
+    fund_sections: str | None
+    deadline: date | None
+    category: str | None
+    bonus: NonNegativeInt | None
+    location: str | None
+    link: str | None
+    description: str | None
