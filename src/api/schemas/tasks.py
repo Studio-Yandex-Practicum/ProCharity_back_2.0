0a430f0199f6ce33308a8838b57504b855b3f9e2
Евгений Москалянов <emos@inbox.ru>
2024-04-27 22:42:38 +0300

    В схемы для модели Task добавлено поле fund_link + поправлены примеры.

--- src/api/schemas/tasks.py
+++ src/api/schemas/tasks.py
@@ -9,16 +9,21 @@ from src.api.schemas.base import RequestBase, ResponseBase
 class TaskCommonFieldsMixin:
     """Набор общих полей для схем модели Task."""
 
-    title: str = Field(..., examples=["Task Title"], description="Название задачи.")
-    name_organization: str | None = Field(None, examples=["My Fund"], description="Название Фонда.")
+    title: str = Field(..., examples=["Example Task"], description="Название задачи.")
+    name_organization: str | None = Field(None, examples=["Example Fund"], description="Название Фонда.")
     legal_address: str | None = Field(None, examples=["Fund Legal Address"], description="Юридический адрес Фонда.")
     fund_city: str | None = Field(None, examples=["Fund City"], description="Фактический адрес Фонда.")
     fund_rating: StrictFloat | None = Field(None, examples=[78.65], description="Рейтинг Фонда.")
     fund_site: str | None = Field(
-        None, examples=["https://fundexample.com"], description="Страница Фонда в сети Интернет."
+        None, examples=["https://example-fund.com"], description="Страница Фонда в сети Интернет."
     )
-    yb_link: str | None = Field(None, examples=["https://youtubeexample.com"], description="Страница Фонда в Youtube.")
-    vk_link: str | None = Field(None, examples=["https://vkexample.com"], description="Страница Фонда в VK.")
+    fund_link: str | None = Field(
+        None, examples=["https://mainsite.com/funds/1234"], description="Публичная страница Фонда на основном сайте."
+    )
+    yb_link: str | None = Field(
+        None, examples=["https://youtube.com/@example_fund"], description="Страница Фонда в Youtube."
+    )
+    vk_link: str | None = Field(None, examples=["https://vk.com/example_fund"], description="Страница Фонда в VK.")
     fund_sections: str | None = Field(None, examples=["1, 7"], description="Сферы деятельности Фонда.")
     deadline: date | None = Field(
         None, format=DATE_FORMAT, examples=["23.11.2024"], description="Дедлайн выполнения задачи."
@@ -28,7 +33,7 @@ class TaskCommonFieldsMixin:
     )
     bonus: int = Field(..., ge=1, le=10, examples=[5], description="Количество бонусов за выполнение задачи.")
     location: str = Field(..., examples=["Task Location"], description="Место выполнения задачи.")
-    link: str = Field(..., examples=["https://examples.com"], description="Ссылка на страницу задачи.")
+    link: str = Field(..., examples=["https://mainsite.com/tasks/1234"], description="Ссылка на страницу задачи.")
     description: str = Field(..., examples=["Task description"], description="Описание задачи.")
 
 
