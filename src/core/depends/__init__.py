f9a392965aa13ee221f69960cfcd2ad986a6cc44
Евгений Аборкин <newtowngenek@gmail.com>
2024-05-14 17:03:31 +0300

    fix/import PaginateContainer

--- src/core/depends/__init__.py
+++ src/core/depends/__init__.py
@@ -4,6 +4,7 @@ from .bot_services import BotServicesContainer
 from .container import Container
 from .data_base_connection import DataBaseConnectionContainer
 from .jwt_services import JWTServicesContainer
+from .pagination import PaginateContainer
 from .repositories import RepositoriesContainer
 
 __all__ = (
