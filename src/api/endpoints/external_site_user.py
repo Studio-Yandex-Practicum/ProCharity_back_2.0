3ea7ec5e134a93eb76e859d11e80875167fb8879
Евгений Москалянов <emos@inbox.ru>
2024-05-24 20:42:37 +0300

    Добавлен эндпойнт для удаления пользователя сайта ProCharity из БД бота.

--- src/api/endpoints/external_site_user.py
+++ src/api/endpoints/external_site_user.py
@@ -1,5 +1,5 @@
 from dependency_injector.wiring import Provide, inject
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status
 
 from src.api.auth import check_header_contains_token
 from src.api.schemas import ExternalSiteFundRequest, ExternalSiteVolunteerRequest
@@ -16,6 +16,19 @@ async def external_user_registration(
     site_user_service: ExternalSiteUserService = Depends(Provide[Container.api_services_container.site_user_service]),
 ) -> None:
     await site_user_service.register(site_user)
+
+
+@site_user_router.delete(
+    "/external_user_registration/{user_id}",
+    status_code=status.HTTP_204_NO_CONTENT,
+    description="Удаляет данные пользователя сайта ProCharity из БД бота.",
+)
+@inject
+async def external_user_delete(
+    user_id: int,
+    site_user_service: ExternalSiteUserService = Depends(Provide[Container.api_services_container.site_user_service]),
+) -> None:
+    await site_user_service.archive(user_id)
 
 
 @site_user_router.post("/external_fund_registration", description="Актуализирует данные фонда с сайта ProCharity.")
