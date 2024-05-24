5d35c4f23ea1a12982c67e5bbb19869b6232a875
Евгений Москалянов <emos@inbox.ru>
2024-05-24 20:39:35 +0300

    В api.ExternalSiteUserService добавлен фейковый метод для архивирования.

--- src/api/services/external_site_user.py
+++ src/api/services/external_site_user.py
@@ -33,3 +33,6 @@ class ExternalSiteUserService:
 
             await self._user_repository.update(user.id, user)
             await self._user_repository.set_categories_to_user(user.id, site_user.specializations)
+
+    async def archive(self, external_id: int):
+        await self._site_user_repository.get_by_external_id(external_id)
