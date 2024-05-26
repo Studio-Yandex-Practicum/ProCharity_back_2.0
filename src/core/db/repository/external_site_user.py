38cae08bda2ed9c8abaeb2e4d354ac20434472c5
Евгений Москалянов <emos@inbox.ru>
2024-05-26 21:27:36 +0300

    Рефакторинг: вынесение повторяющегося кода в отдельный метод.

--- src/core/db/repository/external_site_user.py
+++ src/core/db/repository/external_site_user.py
@@ -1,4 +1,4 @@
-from sqlalchemy import false, select
+from sqlalchemy import Select, false, select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.core.db.models import ExternalSiteUser
@@ -12,12 +12,10 @@ class ExternalSiteUserRepository(AbstractRepository):
     def __init__(self, session: AsyncSession) -> None:
         super().__init__(session, ExternalSiteUser)
 
-    async def get_by_id_hash(self, id_hash: str, with_archived=False) -> ExternalSiteUser | None:
+    async def get_by_id_hash(self, id_hash: str, with_archived: bool = False) -> ExternalSiteUser | None:
         """Возвращает пользователя (или None) по id_hash."""
         statement = select(ExternalSiteUser).where(ExternalSiteUser.id_hash == id_hash)
-        if not with_archived:
-            statement = statement.where(self._model.is_archived == false())
-        return await self._session.scalar(statement)
+        return await self._session.scalar(self._add_test_not_archived_to_select(statement, not with_archived))
 
     async def get_or_create_by_id_hash(self, id_hash: str) -> tuple[ExternalSiteUser, bool]:
         """Возвращает или создает пользователя по id_hash."""
@@ -26,14 +24,14 @@ class ExternalSiteUserRepository(AbstractRepository):
             return (instance, False)
         return await self.create(ExternalSiteUser(id_hash=id_hash)), True
 
-    async def get_by_external_id_or_none(self, external_id: int, with_archived=False) -> ExternalSiteUser | None:
+    async def get_by_external_id_or_none(
+        self, external_id: int, with_archived: bool = False
+    ) -> ExternalSiteUser | None:
         """Возвращает пользователя (или None) по external_id."""
         statement = select(self._model).where(self._model.external_id == external_id)
-        if not with_archived:
-            statement = statement.where(self._model.is_archived == false())
-        return await self._session.scalar(statement)
+        return await self._session.scalar(self._add_test_not_archived_to_select(statement, not with_archived))
 
-    async def get_by_external_id(self, external_id: int, with_archived=False) -> ExternalSiteUser:
+    async def get_by_external_id(self, external_id: int, with_archived: bool = False) -> ExternalSiteUser:
         """Возвращает пользователя по external_id, а в случае его отсутствия
         возбуждает исключение NotFoundException.
         """
@@ -47,3 +45,7 @@ class ExternalSiteUserRepository(AbstractRepository):
         instance.is_archived = True
         instance.user = None
         await self.update(instance.id, instance)
+
+    def _add_test_not_archived_to_select(self, statement: Select, add_test: bool) -> Select:
+        """Добавляет к оператору SELECT проверку условия is_archived = False."""
+        return statement.where(self._model.is_archived == false()) if add_test else statement
