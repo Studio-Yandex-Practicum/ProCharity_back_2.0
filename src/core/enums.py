f4de3ca40f8471270a041d60a560d933da260189
Максим Трухинов <maks.truxinov@yandex.ru>
2024-06-27 01:06:26 +0300

    Added Docstring to UserRoles

--- src/core/enums.py
+++ src/core/enums.py
@@ -12,6 +12,13 @@ class TelegramNotificationUsersGroups(StrEnum):
 
 class UserRoles(StrEnum):
     """Роли пользователя в системе.
+    Класс с доступными статусами модерации пользователей:
+
+    - NEW_VOL: Новый волонтер / фонд
+    - WAIT: Ожидает модерации
+    - MODERATED: Промодерирован
+    - NO_MODERATED: Не прошел модерацию
+    - BLOCKED: Заблокирован
 
     Значения членов перечисления - строки длиной не более
     src.core.db.models.MAX_USER_ROLE_NAME_LENGTH.
