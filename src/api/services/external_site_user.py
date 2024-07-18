ad99b33a4f34d925c4edf2ac26c834b4cc09d845
Genek91 <newtowngenek@gmail.com>
2024-07-18 16:23:50 +0300

    add_external_user_registration_endpoints_patch

--- src/api/services/external_site_user.py
+++ src/api/services/external_site_user.py
@@ -1,7 +1,14 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.api.schemas import ExternalSiteFundRequest, ExternalSiteVolunteerRequest
+from src.api.schemas import (
+    ExternalSiteFundPartialUpdate,
+    ExternalSiteFundRequest,
+    ExternalSiteVolunteerPartialUpdate,
+    ExternalSiteVolunteerRequest,
+)
+from src.core.db.models import ExternalSiteUser
 from src.core.db.repository import ExternalSiteUserRepository, UserRepository
+from src.core.enums import UserRoles
 from src.core.exceptions import BadRequestException
 
 
@@ -39,6 +46,25 @@ class ExternalSiteUserService:
 
             await self._user_repository.update(user.id, user)
             await self._user_repository.set_categories_to_user(user.id, site_user.specializations)
+
+    async def partial_update(
+        self, external_id: int, site_user_schema: ExternalSiteVolunteerPartialUpdate | ExternalSiteFundPartialUpdate
+    ) -> None:
+        """Обновляет данные/часть данных существующего пользователя сайта."""
+        data_for_update = site_user_schema.model_dump(exclude_none=True)
+        site_user = await self._site_user_repository.get_by_external_id(external_id, False)
+
+        await self._site_user_repository.update(site_user.id, ExternalSiteUser(**data_for_update))
+
+        user = await self._user_repository.get_by_external_id(site_user.id)
+        if user:
+            for attr, value in data_for_update.items():
+                setattr(user, attr, value)
+
+            await self._user_repository.update(user.id, user)
+
+            if site_user.role == UserRoles.VOLUNTEER and site_user_schema.specializations:
+                await self._user_repository.set_categories_to_user(user.id, site_user.specializations)
 
     async def archive(self, external_id: int) -> None:
         """Архивирует пользователя сайта и удаляет его связь с ботом."""
