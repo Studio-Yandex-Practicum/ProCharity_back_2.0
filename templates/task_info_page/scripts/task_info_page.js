f1b81edd0137aad68bbf62d5999d7efa89bd35e1
Ilya-Ivashchenk0 <donhuilio@yandex.ru>
2024-08-07 00:19:48 +0300

    feat: add files icons

--- templates/task_info_page/scripts/task_info_page.js
+++ templates/task_info_page/scripts/task_info_page.js
@@ -1,129 +1,147 @@
+const icons = {
+  doc: '/static/task_info_page/images/doc.svg',
+  docx: '/static/task_info_page/images/docx.svg',
+  jpg: '/static/task_info_page/images/jpg.svg',
+  pdf: '/static/task_info_page/images/pdf.svg',
+  png: '/static/task_info_page/images/png.svg',
+  ppt: '/static/task_info_page/images/ppt.svg',
+  pptx: '/static/task_info_page/images/pptx.svg',
+  unknown: '/static/task_info_page/images/unknown.svg',
+  xls: '/static/task_info_page/images/xls.svg',
+  xlsx: '/static/task_info_page/images/xlsx.svg'
+}
+
+function getFileIcon(fileName) {
+  const extension = fileName.split('.').pop().toLowerCase()
+  return icons[extension] || icons.unknown
+}
+
 function view_query_parameters(destElementSelector) {
-  const destElement = document.querySelector(destElementSelector);
+  const destElement = document.querySelector(destElementSelector)
   if (destElement) {
-    const searchParams = new URL(document.location).searchParams;
-    let paramsObject = {};
+    const searchParams = new URL(document.location).searchParams
+    let paramsObject = {}
 
     for (const [key, value] of searchParams) {
-      paramsObject[key] = value;
+      paramsObject[key] = value
     }
 
     fetch(`${paramsObject.api_root_path}/task/${paramsObject.id}`)
       .then((response) => {
         if (!response.ok) {
-          throw new Error("Задание неактуально");
+          throw new Error("Задание неактуально")
         }
-        return response.json();
+        return response.json()
       })
       .then((task) => {
 
         if (!task || Object.keys(task).length === 0) {
-          throw new Error("Пустой ответ от сервера");
+          throw new Error("Пустой ответ от сервера")
         }
 
         // Описание задачи
-        const taskInfo = document.querySelector("#task_info");
+        const taskInfo = document.querySelector("#task_info")
         if (taskInfo && task.description_main) {
-          const title = document.createElement("h1");
-          title.textContent = "Описание задачи";
+          const title = document.createElement("h1")
+          title.textContent = "Описание задачи"
 
-          const ul = document.createElement("ul");
+          const ul = document.createElement("ul")
 
           task.description_main.forEach((item) => {
-            const li = document.createElement("li");
+            const li = document.createElement("li")
 
-            const subtitle = document.createElement("h3");
-            subtitle.textContent = item.title;
+            const subtitle = document.createElement("h3")
+            subtitle.textContent = item.title
 
-            const description = document.createElement("p");
-            description.textContent = item.value;
+            const description = document.createElement("p")
+            description.textContent = item.value
 
-            li.appendChild(subtitle);
-            li.appendChild(description);
-            ul.appendChild(li);
-          });
+            li.appendChild(subtitle)
+            li.appendChild(description)
+            ul.appendChild(li)
+          })
 
-          taskInfo.appendChild(title);
-          taskInfo.appendChild(ul);
+          taskInfo.appendChild(title)
+          taskInfo.appendChild(ul)
         }
 
         // Ссылки
-        const descriptionLinks = document.querySelector("#description_links");
+        const descriptionLinks = document.querySelector("#description_links")
         if (descriptionLinks && task.description_links) {
-          const title = document.createElement("h2");
-          title.textContent = "Ссылки";
-          const ul = document.createElement("ul");
+          const title = document.createElement("h2")
+          title.textContent = "Ссылки"
+          const ul = document.createElement("ul")
 
           task.description_links.forEach((link) => {
-            const li = document.createElement("li");
-            const linkIcon = document.createElement("img");
-            linkIcon.src = "/static/task_info_page/images/link.svg";
-            linkIcon.alt = "";
-            linkIcon.className = "link-icon";
-            const a = document.createElement("a");
-            a.href = link.link;
-            a.target = "_blank";
-            a.textContent = link.name;
-            li.appendChild(linkIcon);
-            li.appendChild(a);
-            ul.appendChild(li);
-          });
+            const li = document.createElement("li")
+            const linkIcon = document.createElement("img")
+            linkIcon.src = "/static/task_info_page/images/link.svg"
+            linkIcon.alt = ""
+            linkIcon.className = "link-icon"
+            const a = document.createElement("a")
+            a.href = link.link
+            a.target = "_blank"
+            a.textContent = link.name
+            li.appendChild(linkIcon)
+            li.appendChild(a)
+            ul.appendChild(li)
+          })
 
-          descriptionLinks.appendChild(title);
-          descriptionLinks.appendChild(ul);
+          descriptionLinks.appendChild(title)
+          descriptionLinks.appendChild(ul)
         }
 
         // Файлы
-        const descriptionFiles = document.querySelector("#description_files");
+        const descriptionFiles = document.querySelector("#description_files")
         if (descriptionFiles && task.description_files) {
-          const title = document.createElement("h2");
-          title.textContent = "Файлы";
-          const ul = document.createElement("ul");
+          const title = document.createElement("h2")
+          title.textContent = "Файлы"
+          const ul = document.createElement("ul")
 
           task.description_files.forEach((file) => {
             const size =
               file.file_size > 1024 * 1024
                 ? `${(file.file_size / (1024 * 1024)).toFixed(2)} Мб`
-                : `${(file.file_size / 1024).toFixed(2)} Кб`;
-            const li = document.createElement("li");
-            const fileIcon = document.createElement("img");
-            fileIcon.src = "/static/task_info_page/images/file.svg";
-            fileIcon.alt = "";
-            fileIcon.className = "file-icon";
-            const a = document.createElement("a");
-            a.href = file.file_link;
-            a.target = "_blank";
-            a.textContent = `${file.file_name}`;
-            const span = document.createElement("span");
-            span.textContent = size;
-            li.appendChild(fileIcon);
-            li.appendChild(a);
-            li.appendChild(span);
-            ul.appendChild(li);
-          });
+                : `${(file.file_size / 1024).toFixed(2)} Кб`
+            const li = document.createElement("li")
+            const fileIcon = document.createElement("img")
+            fileIcon.src = getFileIcon(file.file_name)
+            fileIcon.alt = ""
+            fileIcon.className = "file-icon"
+            const a = document.createElement("a")
+            a.href = file.file_link
+            a.target = "_blank"
+            a.textContent = `${file.file_name}`
+            const span = document.createElement("span")
+            span.textContent = size
+            li.appendChild(fileIcon)
+            li.appendChild(a)
+            li.appendChild(span)
+            ul.appendChild(li)
+          })
 
-          descriptionFiles.appendChild(title);
-          descriptionFiles.appendChild(ul);
+          descriptionFiles.appendChild(title)
+          descriptionFiles.appendChild(ul)
         }
 
         // Бонус от фонда
-        const descriptionBonus = document.querySelector("#description_bonus");
+        const descriptionBonus = document.querySelector("#description_bonus")
         if (descriptionBonus && task.description_bonus) {
-          const title = document.createElement("h2");
-          title.textContent = "Бонус от фонда";
-          const p = document.createElement("p");
-          p.textContent = task.description_bonus;
-          descriptionBonus.appendChild(title);
-          descriptionBonus.appendChild(p);
+          const title = document.createElement("h2")
+          title.textContent = "Бонус от фонда"
+          const p = document.createElement("p")
+          p.textContent = task.description_bonus
+          descriptionBonus.appendChild(title)
+          descriptionBonus.appendChild(p)
         }
       })
       .catch((error) => {
-        console.error("Error:", error);
-        const p = document.createElement("p");
-        p.textContent = error.message;
-        destElement.appendChild(p);
-      });
+        console.error("Error:", error)
+        const p = document.createElement("p")
+        p.textContent = error.message
+        destElement.appendChild(p)
+      })
   }
 }
 
-view_query_parameters("main");
+view_query_parameters("main")
