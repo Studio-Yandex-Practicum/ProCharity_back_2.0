d85461f2d7887965fb86a105455ff80bb4bc38d2
Olga Avdeeva <gorskyolga@gmail.com>
2024-08-26 11:36:32 +0200

    revert last changes

--- src/api/services/analytics.py
+++ src/api/services/analytics.py
@@ -1,7 +1,7 @@
 from datetime import timedelta
 
 from src.api.constants import DAYS_NUMBER_FOR_USERS_STATISTIC
-from src.core.db.repository import ExternalSiteUserRepository, UnsubscribeReasonRepository, UserRepository
+from src.core.db.repository import UnsubscribeReasonRepository, UserRepository
 
 
 class AnalyticsService:
@@ -11,11 +11,9 @@ class AnalyticsService:
         self,
         user_repository: UserRepository,
         unsubscribe_reason_repository: UnsubscribeReasonRepository,
-        site_user_repository: ExternalSiteUserRepository,
     ) -> None:
         self._user_repository: UserRepository = user_repository
         self._unsubscribe_reason_repository: UnsubscribeReasonRepository = unsubscribe_reason_repository
-        self._site_user_repository: ExternalSiteUserRepository = site_user_repository
 
     async def get_user_number(self) -> None:
         return await self._user_repository.count_all()
@@ -31,8 +29,8 @@ class AnalyticsService:
 
     async def get_added_external_users_statistic(self, date_limit) -> dict[str, int]:
         date_begin = date_limit - timedelta(days=DAYS_NUMBER_FOR_USERS_STATISTIC - 1)
-        added_external_users = await self._site_user_repository.get_statistics_by_days(
-            date_begin, date_limit, "created_at"
+        added_external_users = await self._user_repository.get_statistics_by_days(
+            date_begin, date_limit, "external_signup_date"
         )
         return added_external_users
 
