ee89c19cca38c7ef257842d8f037c153347ae09d
Alexander Timchenko <astimch2022@gmail.com>
2024-09-01 19:08:26 +0300

    #627: fix subscribe messages

--- src/bot/constants/enum.py
+++ src/bot/constants/enum.py
@@ -3,10 +3,9 @@ import enum
 
 class REASONS(enum.StrEnum):
     to_much_messages = "Слишком много уведомлений"
-    no_time = "Нет времени на волонтёрство"
+    no_time = "Нет времени на волонтерство"
     no_match = "Нет подходящих заданий"
-    uncomfortable = "Бот мне неудобен"
-    funds_dont_choose = "Фонды меня не выбирают"
+    uncomfortable = "Неудобно пользоваться ботом"
     other = "Другое"
 
 
